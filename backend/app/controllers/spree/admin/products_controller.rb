ab4877b6e24b6720b40fa2c9de9ce299c2599e41
DangerDawson <david.dawson@gmail.com>
2013-07-18 18:05:01 +0100

    Revert "Prevent users from deleting parts"
    This reverts commit 8481d4902f6525be36161bc258151b57672a1426.

--- backend/app/controllers/spree/admin/products_controller.rb
+++ backend/app/controllers/spree/admin/products_controller.rb
@@ -29,17 +29,13 @@ module Spree
 
       def destroy
         @product = Product.find_by_permalink!(params[:id])
-        if @product.variants_including_master.detect { |v| v.part? == true }
-          flash[:error] = Spree.t('notice_messages.product_is_part_of_an_assembly')
-          respond_with(@collection)
-        else
-          @product.destroy
-          flash[:success] = Spree.t('notice_messages.product_deleted')
+        @product.destroy
 
-          respond_with(@product) do |format|
-            format.html { redirect_to collection_url }
-            format.js  { render_js_for_destroy }
-          end
+        flash[:success] = Spree.t('notice_messages.product_deleted')
+
+        respond_with(@product) do |format|
+          format.html { redirect_to collection_url }
+          format.js  { render_js_for_destroy }
         end
       end
 
--- backend/app/controllers/spree/admin/variants_controller.rb
+++ backend/app/controllers/spree/admin/variants_controller.rb
@@ -8,22 +8,15 @@ module Spree
       # instead of actually deleting the product.
       def destroy
         @variant = Variant.find(params[:id])
-
-        if @variant.part? == true 
-          flash[:error] = Spree.t('notice_messages.variant_is_part_of_an_assembly')
-          respond_with(@collection)
+        if @variant.destroy
+          flash[:success] = Spree.t('notice_messages.variant_deleted')
         else
+          flash[:success] = Spree.t('notice_messages.variant_not_deleted')
+        end
 
-          if @variant.destroy
-            flash[:success] = Spree.t('notice_messages.variant_deleted')
-          else
-            flash[:success] = Spree.t('notice_messages.variant_not_deleted')
-          end
-
-          respond_with(@variant) do |format|
-            format.html { redirect_to admin_product_variants_url(params[:product_id]) }
-            format.js  { render_js_for_destroy }
-          end
+        respond_with(@variant) do |format|
+          format.html { redirect_to admin_product_variants_url(params[:product_id]) }
+          format.js  { render_js_for_destroy }
         end
       end
 
