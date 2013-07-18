8481d4902f6525be36161bc258151b57672a1426
DangerDawson <david.dawson@gmail.com>
2013-07-18 17:54:15 +0100

    Prevent users from deleting parts

--- backend/app/controllers/spree/admin/products_controller.rb
+++ backend/app/controllers/spree/admin/products_controller.rb
@@ -29,13 +29,17 @@ module Spree
 
       def destroy
         @product = Product.find_by_permalink!(params[:id])
-        @product.destroy
+        if @product.variants_including_master.detect { |v| v.part? == true }
+          flash[:error] = Spree.t('notice_messages.product_is_part_of_an_assembly')
+          respond_with(@collection)
+        else
+          @product.destroy
+          flash[:success] = Spree.t('notice_messages.product_deleted')
 
-        flash[:success] = Spree.t('notice_messages.product_deleted')
-
-        respond_with(@product) do |format|
-          format.html { redirect_to collection_url }
-          format.js  { render_js_for_destroy }
+          respond_with(@product) do |format|
+            format.html { redirect_to collection_url }
+            format.js  { render_js_for_destroy }
+          end
         end
       end
 
