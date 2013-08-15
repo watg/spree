c5f4dc456f4ec933fb3fbfc948c0373fad08d986
DangerDawson <david.dawson@gmail.com>
2013-08-15 17:52:55 +0100

    - fixed a bug where you can not deselect all the variants

--- backend/app/services/spree/displayable_variants_service.rb
+++ backend/app/services/spree/displayable_variants_service.rb
@@ -1,18 +1,10 @@
 module Spree
   class DisplayableVariantsService < Struct.new(:controller)
     def perform(product_id, variant_ids)
-      product = Spree::Product.find(product_id)
-      current_ids = Spree::DisplayableVariant.where(product_id: product.id).uniq.pluck(:variant_id)
-      
-      variants_to_add(current_ids, variant_ids).each do |variant_id|
-        product.taxons.each do |taxon|
-          Spree::DisplayableVariant.create(product_id: product_id, variant_id: variant_id, taxon_id: taxon.id)
-        end
-      end
 
-      to_remove = variants_to_remove(current_ids, variant_ids)
-      Spree::DisplayableVariant.destroy_all(product_id: product_id, variant_id: to_remove) unless to_remove.blank?
-      
+      product = Spree::Product.find(product_id)
+      update_displayable_variants(product,variant_ids)
+     
       controller.send(:update_success, product)
  
     rescue Exception => error
@@ -22,11 +14,33 @@ module Spree
 
 
     private
-    def variants_to_remove(current_ids, variant_ids=[])
+
+    def update_displayable_variants( product, variant_ids )
+      current_ids = Spree::DisplayableVariant.where(product_id: product.id).uniq.pluck(:variant_id)
+
+      variants_to_add(current_ids, variant_ids).each do |variant_id|
+        all_taxons( product.taxons ).each do |taxon|
+          Spree::DisplayableVariant.create(product_id: product.id, variant_id: variant_id, taxon_id: taxon.id)
+        end
+      end
+
+      to_remove = variants_to_remove(current_ids, variant_ids)
+      Spree::DisplayableVariant.destroy_all(product_id: product.id, variant_id: to_remove) unless to_remove.blank?
+    end
+
+    def all_taxons(taxons)
+      taxons.map do |t|
+        t.self_and_parents
+      end.flatten
+    end
+
+    def variants_to_remove(current_ids, variant_ids)
+      variant_ids ||= []
       current_ids - variant_ids.map(&:to_i)
     end
 
-    def variants_to_add(current_ids, variant_ids=[])
+    def variants_to_add(current_ids, variant_ids)
+      variant_ids ||= []
       variant_ids.map(&:to_i) - current_ids
     end
   end
