4bbf73435f730490ae93bfdab638ad3db2950da8
Jerome Riga <jerome@woolandthegang.com>
2013-08-27 13:27:00 +0100

    merge order content

--- core/app/models/spree/order_contents.rb
+++ core/app/models/spree/order_contents.rb
@@ -6,13 +6,19 @@ module Spree
       @order = order
     end
 
-    # Get current line item for variant if exists
-    # Add variant qty to line_item
-    def add(variant, quantity, currency=nil, shipment=nil)
-      line_item = order.find_line_item_by_variant(variant)
-      add_to_line_item(line_item, variant, quantity, currency, shipment)
+    #  Sale feature
+    ## Transfert from spree extension product-assembly with options
+    #
+    def add(variant, quantity, currency=nil, shipment=nil, options=nil)
+      options_with_qty = (options.blank? ? [] : options)
+      #get current line item for variant if exists
+      line_item = Spree::LineItem.find_by_order_variant_options(order,variant,options_with_qty)
+      
+      #add variant qty to line_item
+      add_to_line_item(line_item, variant, quantity, currency, shipment, options_with_qty)
     end
-
+    ######
+    
     # Get current line item for variant
     # Remove variant qty from line_item
     def remove(variant, quantity, shipment=nil)
@@ -24,32 +30,42 @@ module Spree
 
       remove_from_line_item(line_item, variant, quantity, shipment)
     end
-
+    
     private
 
-    def add_to_line_item(line_item, variant, quantity, currency=nil, shipment=nil)
+    #  Sale feature
+    ## Transfert from spree extension product-assembly with options
+    #
+    def add_to_line_item(line_item, variant, quantity, currency=nil, shipment=nil, options=nil)
+      currency ||= Spree::Config[:currency] # default to that if none is provided
+      
       if line_item
         line_item.target_shipment = shipment
-        line_item.quantity += quantity.to_i
-        line_item.currency = currency unless currency.nil?
+        line_item.quantity       += quantity
+        line_item.currency        = currency 
         line_item.save
       else
-        line_item = LineItem.new(quantity: quantity)
+        line_item = Spree::LineItem.new(quantity: quantity)
         line_item.target_shipment = shipment
-        line_item.variant = variant
-        if currency
-          line_item.currency = currency unless currency.nil?
-          line_item.price    = variant.price_in(currency).amount
-        else
-          line_item.price    = variant.price
+        line_item.variant         = variant
+        line_item.currency        = currency
+        line_item.price           = variant.current_price_in(currency).amount
+
+        if variant.in_sale?
+          line_item.in_sale       = variant.in_sale
+          line_item.normal_price  = variant.price_normal_in(currency).amount
         end
+        
+        line_item.add_options(options,currency) unless options.blank?
+        
         order.line_items << line_item
         line_item
       end
-
+      
       order.reload
       line_item
     end
+    ###### 
 
     def remove_from_line_item(line_item, variant, quantity, shipment=nil)
       line_item.quantity += -quantity
