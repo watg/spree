d21290f7ca0320ae0fae06a828af968415d152c6
Jerome Riga <jerome@woolandthegang.com>
2013-08-29 21:11:30 +0100

    [VO] find variant by product and option values

--- core/app/models/spree/variant.rb
+++ core/app/models/spree/variant.rb
@@ -60,13 +60,21 @@ module Spree
 
     scope :available, lambda { joins(:product).where("spree_products.available_on <= ?", Time.zone.now)  }
 
-    def self.active(currency = nil)
-      joins(:prices).where(deleted_at: nil).where('spree_prices.currency' => currency || Spree::Config[:currency]).where('spree_prices.amount IS NOT NULL')
-    end
+    class << self
+      def active(currency = nil)
+        joins(:prices).where(deleted_at: nil).where('spree_prices.currency' => currency || Spree::Config[:currency]).where('spree_prices.amount IS NOT NULL')
+      end
+      
+      def displayable(product_id)
+        where(product_id: product_id, is_master: false).includes(:displayable_variants)
+      end
 
-    def self.displayable(product_id)
-      where(product_id: product_id, is_master: false).includes(:displayable_variants)
-    end      
+      def options_by_product(product, option_value_name_list)
+        _option_values = Spree::OptionValue.select(:id).where(name: option_value_name_list).all.map(&:id).compact.sort
+
+        product.variants.detect {|v| v.option_values.map(&:id).sort == _option_values}
+      end
+    end
 
     def visible?
       displayable_variants.any?
