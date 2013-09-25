e81320d98e84fef12f51a13daeaaa2416a8bf3b5
mtomov <mtomov@outlook.com>
2013-09-25 20:06:26 +0100

    pinterest functionality works, needs specs

---
+++ api/app/controllers/spree/api/pinterest_controller.rb
@@ -0,0 +1,71 @@
+module Spree
+  module Api
+    class PinterestController < Spree::Api::BaseController
+     
+      def show
+        pinterest_rich_pin = PinterestRichPin.new(params[:url])
+        
+        if pinterest_rich_pin.valid?
+          @pinterest = pinterest_rich_pin.get_result
+          render json: @pinterest   
+        # else
+        #   @pinterest = ""
+        #   render json: "error"
+        end
+      end
+
+
+    end
+  end
+
+  class PinterestRichPin
+    def initialize(url)
+      @product_url = url
+    end
+
+    def valid?
+      url = @product_url.split("/")
+      @product_permalink = url[url.index("products") + 1] if url
+      return false unless @product_permalink
+      variant_option_values = url[url.index("products") + 2, 5]
+      
+      load_product(@product_permalink)
+      return false unless @product
+      load_selected_variant(variant_option_values)
+      return false unless @variant
+      true
+    end
+
+    def get_result
+      OpenStruct.new({
+        provider_name: "Wool and the Gang",
+        url: @product_url,
+        title: @product.name,
+        description: @product.description,
+        product_id: @product_permalink,
+        price: @variant.price,
+        currency_code: @variant.currency,
+        availability: @variant.in_stock? ? "in stock" : "out of stock"
+      })
+    end
+
+  private    
+      
+    def load_product(permalink)
+      @product = Product.find_by_permalink!(permalink)
+    end
+
+    def load_selected_variant(variant_option_values)
+      if variant_option_values.blank?
+        @variant = @product.master
+      else
+        @variant = Spree::Variant.options_by_product(@product, variant_option_values)
+      end
+      
+      if !variant_option_values.blank? && (@variant.blank? || @variant.is_master)
+        return false
+      end
+    end
+
+  end
+end
