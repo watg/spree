eee6293ab8231da1f32d5cf1436ec6311abccd44
Jerome Riga <jerome@woolandthegang.com>
2013-09-25 18:54:54 +0100

    [METAPACK]: parse soap response

--- backend/lib/metapack/client.rb
+++ backend/lib/metapack/client.rb
@@ -1,7 +1,7 @@
 module Metapack
   class Client
     def create_and_allocate_consignment(consignment)
-      url = Metapack::Config.service_base_url + "/allocationService"
+      url = Metapack::Config.service_base_url + "/AllocationService"
       Net::HTTP.start(Metapack::Config.host) {|http|
         req = Net::HTTP::Post.new(url)
         req.basic_auth Metapack::Config.username, Metapack::Config.password
@@ -11,10 +11,6 @@ module Metapack
         req["Content-Type"]  = "text/xml;charset=UTF-8"
         req["Content-Length"]= body.size
 
-        Rails.logger.info '-'*80
-        Rails.logger.info body
-        Rails.logger.info '-'*80
-        
         req.body = body
         extract(http.request(req))
       }
@@ -27,35 +23,29 @@ module Metapack
     end
 
     def extract(response)
-      # check http status code 200 or raise error
-      # consignment_code =
-      # response.body[:create_and_allocate_consignments_response]
-      #              [:create_and_allocate_consignments_return]
-      #              [:create_and_allocate_consignments_return]
-      #              [:consignment_code]
-
-      Rails.logger.info '+'*80
-      Rails.logger.info response.body.inspect
-      Rails.logger.info '+'*80
       if response.code.to_i == 200
         build_hash(response.body)
       else
-        Rails.logger.info(response.body.inspect)
+        Rails.logger.info(response.body)
         raise "The request to Metapack failed error code: #{response.code}"
       end
     end
 
     def build_hash(xml)
-      Hash.from_xml(response.body)["soap:Envelope"]["soap:Body"].inject({}) do |result, elem|
-        result[:metapack_consignment_id] = elem["createAndAllocateConsignmentsResponse"]
-        ['createAndAllocateConsignmentsReturn']
-        ['createAndAllocateConsignmentsReturn']
-        ['consignmentCode']
-        result
-      end
+      response = Nokogiri::XML(xml)
+      {
+        metapack_consignment_id: consignment_code(response)
+      }
     rescue Exception => error
-      Rails.logger.info(xml.inspect)
+      Rails.logger.info(error.inspect)
+      Rails.logger.info(error.backtrace)
       raise "Could not parse response from Metapack"
+    end
+
+    def consignment_code(xml_doc)
+      code = xml_doc.xpath('//createAndAllocateConsignmentsReturn/consignmentCode')[0].content
+      raise "Could not retreive consignment code" if code.blank?
+      code 
     end
   end
 end
