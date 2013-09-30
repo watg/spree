62039c371b903ac24fb04fd590e901a29cde387e
Jerome Riga <jerome@woolandthegang.com>
2013-09-30 19:38:45 +0100

    [METAPACK]: updated image sticker fonts

--- backend/app/doc/spree/pdf/base.rb
+++ backend/app/doc/spree/pdf/base.rb
@@ -6,6 +6,12 @@ module Spree
         pdf = create(pdf, order)
         pdf.render
       end
+
+      def self.to_pdf_file(filename, order)
+        pdf = Prawn::Document.new
+        pdf = create(pdf, order)
+        pdf.render_file(filename)
+      end
     end
   end
 end
--- backend/app/doc/spree/pdf/image_sticker.rb
+++ backend/app/doc/spree/pdf/image_sticker.rb
@@ -5,7 +5,11 @@ module Spree
         made_by_gang: File.join(Rails.root, 'app/assets/images/doc/', 'gang.jpg' ),
         made_by_you:  File.join(Rails.root, 'app/assets/images/doc/', 'you.jpg' )
       }
-      
+
+      FONTS = {
+        bold:  File.join(Rails.root, 'app/assets/fonts', 'gillsansmtprobold.ttf'),
+        light: File.join(Rails.root, 'app/assets/fonts', 'gillsansmtprolight.ttf'),
+      }
       STICKER_COORDINATES_BOTTOM_LEFT = {x: 10, y: 40}
       LINE_HEIGHT = 10
       
@@ -16,9 +20,11 @@ module Spree
           pdf.move_down  100
           pdf.text_box   batch_index.to_s, at: [10, initial_y], height: 30, width: 100 if batch_index
           pdf.font_size  50
+          pdf.font(FONTS[:light])
           pdf.text_box   "HELLO", at: [10, (initial_y-100)], height: 55, width: 400
+          pdf.font(FONTS[:bold])
           pdf.font_size  30
-          pdf.text_box   order.shipping_address.firstname, at: [10, (initial_y - 140)], height: 30, width: 400
+          pdf.text_box   firstname(order), at: [10, (initial_y - 140)], height: 30, width: 400
 
           pdf.move_down  100
           pdf.image      made_unique_by(order), width: 550
@@ -28,6 +34,10 @@ module Spree
         private
         def made_unique_by(order)
           order.has_ready_made? ? ASSETS[:made_by_gang] : ASSETS[:made_by_you]
+        end
+
+        def firstname(order)
+          order.shipping_address.firstname.upcase
         end
 
       end
