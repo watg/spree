328c041cb4a9a8127b13c0aa40679bedd95e36ce
Jerome Riga <jerome@woolandthegang.com>
2013-10-10 22:24:42 +0100

    wip

--- core/app/models/spree/product_group.rb
+++ core/app/models/spree/product_group.rb
@@ -4,5 +4,18 @@ module Spree
     validates :name, :presence => true
     
     has_many :products
+
+    def ready_made_products
+# 
+      products.where("product_type is not 'kit'").displayable_variants
+    end
+
+    def kit_products
+      products.where(product_type: :kit)
+    end
+
+    def permalink
+      name.downcase.split(' ').map{|e| (e.blank? ? nil : e) }.compact.join('-')
+    end
   end
 end
