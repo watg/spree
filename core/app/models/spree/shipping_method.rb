fdc8b1dcd4df9ad6a56e7f57909263458dd5e8ce
David Dawson <ddawson@Davids-MacBook-Air.local>
2013-10-21 19:54:11 +0100

    Fixed spree preferences with shipping

--- core/app/models/spree/shipping_method.rb
+++ core/app/models/spree/shipping_method.rb
@@ -34,11 +34,7 @@ module Spree
     end
 
     def self.calculators
-      # puts spree_calculators.send(model_name_without_spree_namespace).inspect
-      # puts "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-      # puts spree_calculators.send(model_name_without_spree_namespace).map{ |c| puts c.inspect; puts Spree::ShippingCalculator.inspect; puts (c < Spree::ShippingCalculator); c < Spree::ShippingCalculator }.inspect
-      # puts "LLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL"
-      spree_calculators.send(model_name_without_spree_namespace).select{ |c| c.name.start_with?("Spree::Calculator::Shipping::") }
+      spree_calculators.send(model_name_without_spree_namespace).select{ |c| c < Spree::ShippingCalculator }
     end
 
     # Some shipping methods are only meant to be set via backend
