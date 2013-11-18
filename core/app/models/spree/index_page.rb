0bce16c916aec41595c1398b5b539235e1089275
mtomov <mtomov@outlook.com>
2013-11-18 14:34:22 +0000

    Index Page -> hstore to simple string for background color
    Former-commit-id: 2b0ee7793ae8beb0c2ef9bca68ea817a50d1ccae [formerly f983f0aa2825ae0cca81cbcf6ed6cdd3070eda29]
    Former-commit-id: 53d68f56e5dee5545307a96cffa4db9d1d3f924f
    Former-commit-id: f474a89aef3fd296c0c739fd6dcedbb73161a13c

--- core/app/models/spree/index_page.rb
+++ core/app/models/spree/index_page.rb
@@ -3,7 +3,6 @@ module Spree
     has_many :items, class_name: 'Spree::IndexPageItem'
     belongs_to :taxon
 
-    store_accessor :properties, :background_color
     validates_presence_of :name
 
     TEMPLATES = [
