8d24490f223ffef411d1827061c9dde769dc6668
Washington Luiz <huoxito@gmail.com>
2013-11-18 21:59:34 -0300

    Fix specs related to package api changes
    Had to modify the order factory which caused a bunch of other specs to
    fail. Hopefully now it has a more reasonable approach which is first
    create line items list then shipments and then update! itself.
    
    Part of #3991
    
    
    Former-commit-id: bd07a34db943f2c3cda851c0940d971c906fea60 [formerly df356ed4e47d6b38b2808e3b93e81edc086f8e53 [formerly f3bdc62ee67941df4ef6c33f35fd9babc744f979]]
    Former-commit-id: d23ba0f1c078b6fc3465ee157e99785335811679
    Former-commit-id: 52728386dd9441b5115e38c58403229543165b58 [formerly 8699ff3a9dcbbc85959c658239da9354f8a5ec77]
    Former-commit-id: 466df6f5add111b3c429512a82cf379307b74941

--- core/spec/models/spree/shipping_calculator_spec.rb
+++ core/spec/models/spree/shipping_calculator_spec.rb
@@ -2,12 +2,22 @@ require 'spec_helper'
 
 module Spree
   describe ShippingCalculator do
-    let(:variant1) { create(:variant, :price => 10, :currency => 'USD') }
-    let(:variant2) { create(:variant, :price => 20, :currency => 'USD') }
-    let(:package) { double(Stock::Package,
-                         order: mock_model(Order),
-                         contents: [Stock::Package::ContentItem.new(variant1, 2),
-                           Stock::Package::ContentItem.new(variant2, 1)]) }
+    let(:variant1) { build(:variant, :price => 10, :currency => 'USD') }
+    let(:variant2) { build(:variant, :price => 20, :currency => 'USD') }
+
+    let(:line_item1) { build(:line_item, variant: variant1) }
+    let(:line_item2) { build(:line_item, variant: variant2) }
+
+    let(:package) do
+      double(
+        Stock::Package,
+        order: mock_model(Order),
+        contents: [
+          Stock::Package::ContentItem.new(line_item1, variant1, 2),
+          Stock::Package::ContentItem.new(line_item2, variant2, 1)
+        ]
+      )
+    end
 
     subject { ShippingCalculator.new }
 
