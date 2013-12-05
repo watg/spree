4ee78463dfee09823f7693a5b9ca39911a08b3ee
mtomov <mtomov@outlook.com>
2013-12-05 15:43:53 +0000

    new product page form has only two fields - name and title
    Former-commit-id: c36f6451b5a05fcd1bc3edd7a6dbf549ecfe2dab [formerly 9e00ad4de77200040d8953207f005957828cb4c1]
    Former-commit-id: a7cb09765c851b7d005a141da0bc17ecec3f8311
    Former-commit-id: 585b2e42fb18d742ab6681ee6a403ea975969445

--- core/app/models/spree/product_page.rb
+++ core/app/models/spree/product_page.rb
@@ -30,7 +30,7 @@ module Spree
     has_many :index_pages, through: :index_page_items
     belongs_to :target
 
-    before_save :set_permalink
+    before_validation :set_permalink
     after_create :create_tabs
 
     accepts_nested_attributes_for :tabs, allow_destroy: true
@@ -99,6 +99,10 @@ module Spree
 
     private
     def set_permalink
+      puts "++++++++++++++++++++++++++++++++++++"
+      puts "++++++++++++++++++++++++++++++++++++"
+      puts "++++++++++++++++++++++++++++++++++++"
+      puts "++++++++++++++++++++++++++++++++++++"
       if self.permalink.blank? && self.name
         self.permalink = name.downcase.split(' ').map{|e| (e.blank? ? nil : e) }.compact.join('-')
       end
