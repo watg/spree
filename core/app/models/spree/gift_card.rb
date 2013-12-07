c9b55f21c375cc7510c146f5df980c92c134a2cc
Jerome Riga <jerome@woolandthegang.com>
2013-12-07 09:09:02 +0000

    Gift Card model and product type gift_card
    https://trello.com/c/XwukRlFF/2-core-add-new-product-type-gift-card
    
    Former-commit-id: 2dfbc0df616f732ee5c2c0346b969d8e87f66232 [formerly f11f577507aabd10de0d6e3bf6602c6089bf55f0]
    Former-commit-id: 1213efae321b33e8309ed6d6c6bec4adef3d4f7e
    Former-commit-id: 4231c3678bafb2c22c45b0be1f2c959e909710f0

---
+++ core/app/models/spree/gift_card.rb
@@ -0,0 +1,50 @@
+module Spree
+  class GiftCard < ActiveRecord::Base
+    acts_as_paranoid
+    
+    belongs_to :variant
+    belongs_to :buyer_order, class_name: 'Spree::Order'
+    belongs_to :beneficiary_order,  class_name: 'Spree::Order'
+
+    before_validation :creation_setup
+    before_create     :generate_code
+
+    private
+    def creation_setup
+      self.expiry_date = 1.year.from_now        if self.expiry_date.blank?
+      self.buyer_email = self.buyer_order.email if self.buyer_email.blank? 
+    end
+
+    def generate_code
+      if self.code.blank? 
+        self.code = [
+                     buyer_email_buyer_order_id,
+                     currency_value_uuid,
+                     expiry_date_time_now
+                    ].join('-')
+      end 
+    end
+
+    def buyer_email_buyer_order_id
+      encode([buyer_email, buyer_order.id].join, 4)
+    end
+
+    def currency_value_uuid
+      encode([currency, value, ::UUID.generate].join, 6)
+    end
+
+    def expiry_date_time_now
+      encode([expiry_date, Time.now].join, 4)
+    end
+    
+    def encode(string, length)
+      Digest::MD5.hexdigest(string).upcase[0,length]
+    end
+
+    def set_expiry_date
+      if self.expiry_date.blank?
+        self.expiry_date = 1.year.from_now
+      end
+    end
+  end
+end
