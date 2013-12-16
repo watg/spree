eeeb52b6b67e8e54ccda56489f427f920af55d52
David Dawson <david.dawson@gmail.com>
2013-12-16 16:11:45 +0000

    Fixed an issue where items that were out of stock were providing the next variant as a master variant sometimes
    Former-commit-id: b5c15ced49b0f5777f901d02dc85601ea21a1a98 [formerly 49a2a61c60e82e7f0e93f0e8dcabcb0d3107f3c0]
    Former-commit-id: b8a373bd604d6e25410473eb1077953f26342e3b
    Former-commit-id: b463c4cf6f9a027ba0d0af5e2cecd49a3c40ed14

--- core/app/models/spree/product_group.rb
+++ core/app/models/spree/product_group.rb
@@ -5,5 +5,15 @@ module Spree
     validates :name, uniqueness: true
     has_and_belongs_to_many :product_pages, join_table: :spree_product_groups_product_pages
 
+    def next_variant_in_stock
+      Spree::Variant.
+        includes(:stock_items, :product).
+        joins('LEFT OUTER JOIN spree_product_groups ON spree_product_groups.id = spree_products.product_group_id').
+        where("spree_product_groups.id = ? AND spree_stock_items.count_on_hand > 0 AND spree_stock_items.count_on_hand < 500 AND spree_products.individual_sale = ?", self, true).
+        where("spree_variants.is_master = ?", false).
+        references(:stock_items, :product).
+        first
+    end
+
   end
 end
