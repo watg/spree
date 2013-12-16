2d2d8085b686d1fc13e619004ef41cc0cf4772ea
Jerome Riga <jerome@woolandthegang.com>
2013-12-16 21:58:57 +0000

    order with only digital items are marked as shipped
    Former-commit-id: 5ec433a841a0d2987e1af716370aa9babcf60b7b [formerly c381c19907298ea57258fc14d0a282e8c3c2dad5]
    Former-commit-id: 52cd3fa65f5ee7350dfd34452d70b8f27d4bb2a1
    Former-commit-id: efc111bb6fdf377ae3d1c14581e628f78353e0f3

---
+++ core/app/jobs/spree/digital_only_order_shipper_job.rb
@@ -0,0 +1,15 @@
+module Spree
+  class DigitalOnlyOrderShipperJob < Struct.new(:order)
+    
+    def perform
+      li_pn_list = order.line_items.map {|li| li.product_nature == 'digital'}
+      order_have_only_digital_line_items = li_pn_list.inject(true) {|result, li_pn| result && li_pn} && (li_pn_list.size > 1)
+
+      if order_have_only_digital_line_items
+        order.shipment_state = 'shipped'
+        order.save(validate: false)
+        order.shipments.map(&:ship)
+      end
+    end
+  end
+end
