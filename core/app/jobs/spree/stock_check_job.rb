3c890331891120bfdc3d6fa99d2283dcddd15996
Jerome Riga <jerome@woolandthegang.com>
2014-01-13 19:01:47 +0000

    job that sets variant in stock cache
    Former-commit-id: f8e844c3908b1baf762aed1aace707cbd4db5298 [formerly 414178b45efcaa5e368114806cd8971b3d247ce7]
    Former-commit-id: 6c7f99b5987197d3d290e321424aa3c30e7a9194
    Former-commit-id: b7a8dce5e7acf4e1adca36a61ffa544428a93f25

--- core/app/jobs/spree/stock_check_job.rb
+++ core/app/jobs/spree/stock_check_job.rb
@@ -1,7 +1,22 @@
 module Spree
   class StockCheckJob < Struct.new(:stock_item)
     def perform
-      #TODO: raise "define me"
+      if stock_item.variant.assemblies.first
+        assembly_list_that_contains_that_variant = Spree::AssembliesPart.where(part_id: stock_item.variant.id).reorder('count').group_by(&:count)
+        assembly_list_that_contains_that_variant.each {|count, assemblies|  update_assembly(count, assemblies) }
+      else
+        result = Spree::Stock::Quantifier.new(stock_item.variant).can_supply?(1)
+        stock_item.variant.update_attributes(in_stock_cache: result)
+      end
+    end
+
+    private
+    def update_assembly(count, assembly_list)
+      result = Spree::Stock::Quantifier.new(stock_item.variant).can_supply?(count)
+      assembly_lists.each do |e|
+        list = (e.assembly_type == 'Spree::Variant' ? [e.assembly] : Spree::Variant.where(product_id: e.assembly_id) )
+        list.each {|obj| obj.update_attributes(result) }
+      end
     end
   end
 end
