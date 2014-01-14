f34596eefa3f526ca30708e3405f4241378dca45
Jerome Riga <jerome@woolandthegang.com>
2014-01-14 12:38:50 +0000

    update stock-check to work with kit
    Former-commit-id: a84c2f0f90666aa5aa328f1700f2e3fad8ce38da [formerly 5127d3a1752fec34f4a710a4b31e89344d6aadd1]
    Former-commit-id: 154d19da39db690d8ebc6f2d15879e5391cdf7ef
    Former-commit-id: 5f106a5761081e79bef9871151a3debf8fb4b37c

--- core/app/jobs/spree/stock_check_job.rb
+++ core/app/jobs/spree/stock_check_job.rb
@@ -1,22 +1,37 @@
 module Spree
   class StockCheckJob < Struct.new(:stock_item)
     def perform
+      variant_in_stock = Spree::Stock::Quantifier.new(stock_item.variant).can_supply?(1)
+      stock_item.variant.update_attributes(in_stock_cache: variant_in_stock)
+
       if stock_item.variant.assemblies.first
-        assembly_list_that_contains_that_variant = Spree::AssembliesPart.where(part_id: stock_item.variant.id, optional: false).reorder('count').group_by(&:count)
-        assembly_list_that_contains_that_variant.each {|count, assemblies|  update_assembly(count, assemblies) }
-      else
-        result = Spree::Stock::Quantifier.new(stock_item.variant).can_supply?(1)
-        stock_item.variant.update_attributes(in_stock_cache: result)
+        if variant_in_stock
+          check_stock_for_kits_using_this_variant(stock_item.variant)
+        else
+          put_all_kits_using_this_variant_out_of_stock(stock_item.variant)
+        end
       end
     end
 
     private
-    def update_assembly(count, assembly_list)
-      result = Spree::Stock::Quantifier.new(stock_item.variant).can_supply?(count)
-      assembly_list.each do |e|
-        list = (e.assembly_type == 'Spree::Variant' ? [e.assembly] : Spree::Variant.where(product_id: e.assembly_id) ).compact
-        list.each {|obj| obj.update_attributes(in_stock_cache: result) }
+    def put_all_kits_using_this_variant_out_of_stock(part)
+      variant_ids = list_of_kit_variants_using(part).map(&:id)
+      Spree::Variant.where(id: variant_ids).update_all(in_stock_cache: false, updated_at: Time.now) if variant_ids
+    end
+
+    def check_stock_for_kits_using_this_variant(part)
+      list_of_kit_variants_using(part, false).each do |kit_variant|
+        kit_in_stock = Spree::Stock::Quantifier.new(kit_variant).can_supply?(1)
+        kit_variant.update_attributes(in_stock_cache: kit_in_stock)
       end
     end
+
+    def list_of_kit_variants_using(part, in_stock_cache=true)
+      grouping = Spree::AssembliesPart.where(part_id: part.id, optional: false).group_by(&:assembly_type)
+      variant_list = (grouping['Spree::Variant'] ? grouping['Spree::Variant'].map(&:assembly_id) : [])
+      product_list = (grouping['Spree::Product'] ? grouping['Spree::Product'].map(&:assembly_id) : [])
+      Spree::Variant.where("id IN (?) OR product_id IN (?) AND in_stock_cache = ?", variant_list, product_list, in_stock_cache)
+    end
+
   end
 end
