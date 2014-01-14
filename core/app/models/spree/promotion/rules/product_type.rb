279b73b9496a158b8f1592b2ef4e8175629d0f31
Martin Tomov <mtomov@outlook.com>
2014-01-14 16:53:11 +0200

    Product Type rule completed
    Former-commit-id: e0be345a9f8204340f4b01214e511114e00e4d78 [formerly ccdba12c40cb6be16daab3fbc74329b5d8baa8ea]
    Former-commit-id: ede161a29c0fc28b0e7bcbf0c7dcb3c8e59cad8c
    Former-commit-id: 0d8811ad300903ea220c2b61f112094181cfd393

--- core/app/models/spree/promotion/rules/product_type.rb
+++ core/app/models/spree/promotion/rules/product_type.rb
@@ -1,15 +1,16 @@
 # A rule to limit a promotion based on product types in the order.
-# Can require any of the product types to be present.
+# Can require all or any of the product types to be present.
+# Valid product types either come from assigned product type or are assingned directly to the rule.
 module Spree
   class Promotion
     module Rules
       class ProductType < PromotionRule
-        has_many :product_types_promotion_rules, foreign_key: 'promotion_rule_id'
+        has_and_belongs_to_many :product_types, class_name: '::Spree::ProductType', join_table: 'spree_product_types_promotion_rules', foreign_key: 'promotion_rule_id'
         validate :only_one_promotion_per_product_type
 
         # scope/association that is used to test eligibility
         def eligible_product_types
-          product_type_promotions.pluck(:product_type)
+          product_types.pluck(:name)
         end
 
         def eligible?(order, options = {})
@@ -17,18 +18,18 @@ module Spree
           order.products.any? {|p| eligible_product_types.include?(p.product_type) }
         end
 
-        def product_ids_string
-          Spree::Product.types.join(',')
-        end
+        # def product_type_ids_string
+        #   product_type_ids.join(',')
+        # end
 
-        def product_ids_string=(s)
-          self.product_ids = s.to_s.split(',').map(&:strip)
-        end
+        # def product_type_ids_string=(s)
+        #   self.product_type_ids = s.to_s.split(',').map(&:strip)
+        # end
 
         private
 
           def only_one_promotion_per_product_type
-            if Spree::Promotion::Rules::ProductType.all.map(&:product_types_promotion_rules).flatten.uniq!
+            if Spree::Promotion::Rules::ProductType.all.map(&:product_types).flatten.uniq!
               errors[:base] << "You can't create two promotions for the same product type"
             end
           end
