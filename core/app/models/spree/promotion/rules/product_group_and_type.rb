e48c89b279e074d403cbb006f293ecb689fdaf5d
David Dawson <david.dawson@gmail.com>
2014-01-19 11:03:44 +0000

    Fixed the promo for group and types
    Former-commit-id: 7401179168780c26a87548c338b5e045957c7af7 [formerly 0346932320dddbdb08f196c59321606177b4eda9]
    Former-commit-id: 1560b165d3b80c79549460c8a5bbc39da80430de
    Former-commit-id: 6714db9a4ae539f3331511844b587ae3e368e31a

--- core/app/models/spree/promotion/rules/product_group_and_type.rb
+++ core/app/models/spree/promotion/rules/product_group_and_type.rb
@@ -8,14 +8,20 @@ module Spree
         has_and_belongs_to_many :product_groups, class_name: '::Spree::ProductGroup', join_table: 'spree_product_groups_promotion_rules', foreign_key: 'promotion_rule_id'
         has_and_belongs_to_many :product_types, class_name: '::Spree::ProductType', join_table: 'spree_product_types_promotion_rules', foreign_key: 'promotion_rule_id'
 
-        
-        MATCH_POLICIES = %w(any all)
-        preference :match_policy, :string, default: MATCH_POLICIES.first
-
         # scope/association that is used to test eligibility
-        def eligible?(order, options = {})
-          eligible_product_group?(order, options) &&
-            eligible_product_type?(order, options)
+        def eligible?(order,options={})
+          return true if eligible_product_groups.empty? and eligible_product_types.empty?
+
+          selector = order.line_items.joins(:product)
+
+          if eligible_product_types.any?
+            selector = selector.where("spree_products.product_type in (?)", eligible_product_types)
+          end
+          if eligible_product_groups.any?
+            selector = selector.where("spree_products.product_group_id in (?)", eligible_product_groups)
+          end
+
+          selector.any?
         end
 
         def product_group_ids_string
@@ -26,26 +32,10 @@ module Spree
           self.product_group_ids = s.to_s.split(',').map(&:strip)
         end
 
-
         private
         
-        def eligible_product_group?(order, options)
-          return true if eligible_product_groups.empty?
-          if preferred_match_policy == 'all'
-            eligible_product_groups.all? {|p| order.product_groups.include?(p) }
-          else
-            order.product_groups.any? {|p| eligible_product_groups.include?(p) }
-          end
-        end
-
-        def eligible_product_type?(order, options)
-          return true if eligible_product_types.empty?
-          order.products.any? {|p| eligible_product_types.include?(p.product_type) }
-        end
-
-
         def eligible_product_groups
-          product_groups
+          product_groups.pluck(:id)
         end
 
         def eligible_product_types
