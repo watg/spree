9ebba148713b4354126fb0387ead4cfae53f752b
David Dawson <david.dawson@gmail.com>
2014-02-03 21:26:40 +0000

    More fixes for the tests
    Former-commit-id: 44e1d2908e38e8b2f2280e2f75522fd25dc14f9a [formerly e444d5bb4685066b16f8a632b47d95d303ed102d]
    Former-commit-id: d04121da724742ca5bb8b7b89810ec94239c1a27
    Former-commit-id: 4c4d347dac9e45a420dd4a977a8609ac039a8bb9

--- core/config/initializers/user_class_extensions.rb
+++ core/config/initializers/user_class_extensions.rb
@@ -11,6 +11,12 @@ Spree::Core::Engine.config.to_prepare do
       belongs_to :ship_address, :class_name => 'Spree::Address'
       belongs_to :bill_address, :class_name => 'Spree::Address'
 
+      before_validation do
+        self.uuid ||= Spree.user_class.generate_token(:uuid) if self.respond_to?(:uuid)
+      end
+
+      validates :uuid, uniqueness: true
+
       # has_spree_role? simply needs to return true or false whether a user has a role or not.
       def has_spree_role?(role_in_question)
         spree_roles.where(:name => role_in_question.to_s).any?
@@ -19,6 +25,24 @@ Spree::Core::Engine.config.to_prepare do
       def last_incomplete_spree_order
         spree_orders.incomplete.where(:created_by_id => self.id).order('created_at DESC').first
       end
+
+      private
+
+      # Generate a friendly string randomically to be used as token.
+      def self.friendly_token
+        SecureRandom.base64(15).tr('+/=', '-_ ').strip.delete("\n")
+      end
+
+      # Generate a token by looping and ensuring does not already exist.
+      def self.generate_token(column)
+        loop do
+          token = friendly_token
+          break token unless where(column => token).first
+        end
+      end
+
+
+
     end
   end
 end
