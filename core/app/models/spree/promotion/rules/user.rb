2a1b69ff3f334cccf33056d413df504c037e0d61
David Dawson <david.dawson@gmail.com>
2014-02-03 21:07:41 +0000

    Fixed all the tests
    Former-commit-id: 721b8ed238a11936bf654edc6a9907ab56ee2eb9 [formerly fd27b75e4d5f23be2c38232da9b5adf415a4f36b]
    Former-commit-id: e42f3bad0f10a94061585b4b6a7d00be44531071
    Former-commit-id: 92ac85f21d405543fd7500b8db11f3126bbb4294

--- core/app/models/spree/promotion/rules/user.rb
+++ core/app/models/spree/promotion/rules/user.rb
@@ -2,14 +2,8 @@ module Spree
   class Promotion
     module Rules
       class User < PromotionRule
-        # if Spree.user_class
-        # do away with the dynamic user class loading as it causes a bug in the promotions
-        belongs_to :user, class_name: 'Spree::User'
-        has_and_belongs_to_many :users, class_name: "Spree::User", join_table: 'spree_promotion_rules_users', foreign_key: 'promotion_rule_id'
-        # else
-        #   belongs_to :user
-        #   has_and_belongs_to_many :users, join_table: 'spree_promotion_rules_users', foreign_key: 'promotion_rule_id'
-        # end
+        belongs_to :user, class_name: Spree.user_class
+        has_and_belongs_to_many :users, class_name: Spree.user_class, join_table: 'spree_promotion_rules_users', foreign_key: 'promotion_rule_id'
 
         def eligible?(order, options = {})
           users.include?(order.user)
