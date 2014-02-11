9bb9c8df5fe33b732b9351a211c18ecfb83882f7
Martin Tomov <mtomov@outlook.com>
2014-02-11 16:05:47 +0000

    Updating incorrect schema.rb and removing test code
    Former-commit-id: f104c60d504dfc800f788f4e950856ec7581cbc5 [formerly 50d3ee537bcbadf50a8cb49aeb8f94f3035b22ad]
    Former-commit-id: aa527a64005085280dfe0a9061f565656e5289c8
    Former-commit-id: 956300f116912bd48ff45605960976d66405e0db

--- backend/app/controllers/spree/admin/products_overview_controller.rb
+++ backend/app/controllers/spree/admin/products_overview_controller.rb
@@ -9,7 +9,6 @@ module Spree
       def update
         params[:products].each do |product|
           if product[:update_flag]
-            d { product }
             p = Spree::Product.find(product[:id])
             p.update_attributes(martin_type_id: product[:martin_type], product_group_id: product[:product_group])
           end
