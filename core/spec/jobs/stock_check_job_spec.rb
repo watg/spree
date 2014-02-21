368717273c2f53f1a80c6dbb1e3350a9536ec561
David Dawson <david.dawson@gmail.com>
2014-02-21 16:12:49 +0000

    Move the job specs into core
    Former-commit-id: 6dadaee70f353324deb6cd4c6c5da104ce0ccf93 [formerly 17466dd526f5d5a527ca41c769e8fb358f9f0bfa [formerly 06f63616b9c457090e976dd91f75310d665df410]]
    Former-commit-id: cd2d25ce2b3bbd9d7e812d039fc3abe0e8b51196
    Former-commit-id: d1285c3fa39611de2b6e6a603dd786fd2c612f6d [formerly 5a1b3a4944e6abb1fb768c901f8437af234cba68]
    Former-commit-id: 25e30fc602d5e37a05a3cf6f5d61fb55b0fc85d7

---
+++ core/spec/jobs/stock_check_job_spec.rb
@@ -0,0 +1,80 @@
+require 'spec_helper'
+
+describe Spree::StockCheckJob do
+  let(:variant_not_part) {create(:variant_with_stock_items)}
+  let(:stock_item) { variant_not_part.stock_items.first }
+  subject  { Spree::StockCheckJob.new(stock_item) }
+
+  
+  describe "#perform" do
+    context "on variant part of NO kit" do
+      before do
+        allow_any_instance_of(Spree::Stock::SimpleQuantifier).
+          to receive(:can_supply?).
+          and_return(true)
+      end
+
+      it "updates just the variant" do
+        expect(stock_item.variant).to receive(:update_attributes).with(in_stock_cache: true)
+        subject.perform
+      end
+    end
+
+    context "on variant part of a kit" do
+      let(:part) { create(:variant_with_stock_items)}
+      subject  { Spree::StockCheckJob.new(part.stock_items.first) }
+      before do 
+        Spree::AssembliesPart.create(part_id: part.id, assembly_id: variant_not_part.id, assembly_type: 'Spree::Variant', count: 4)
+      end
+
+      context "going in stock" do
+        before do
+          allow_any_instance_of(Spree::Stock::SimpleQuantifier).
+            to receive(:can_supply?).
+            and_return(true)
+        end
+
+        it "all of its parts are in stock" do
+          expect(subject).to receive(:check_stock_for_kits_using_this_variant)
+          subject.perform
+        end
+      end
+  
+
+      context "going out of stock" do
+        before do
+          allow_any_instance_of(Spree::Stock::SimpleQuantifier).
+            to receive(:can_supply?).
+            and_return(false)
+        end
+         
+        it "updates part own stock to out of stock" do
+          subject.perform
+          expect(part.reload.in_stock_cache).to eq(false)
+        end
+        
+        it "put out of stock all kit that have that part" do
+          expect(subject).to receive(:put_all_kits_using_this_variant_out_of_stock)
+          subject.perform
+        end
+      end
+
+    
+    end
+
+  end  
+
+  it "ignores any kit that have that part as optional" do
+    arel = double('arel', group_by: {})
+    
+    expect(Spree::AssembliesPart).
+      to receive(:where).
+      with(part_id: variant_not_part.id, optional: false).
+      and_return(arel)
+    
+    subject.send(:list_of_kit_variants_using, variant_not_part)
+  end
+
+  
+
+end
