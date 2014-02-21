12d231eafecb6d5e65ab8e91a3f3ecf2120aa3f4
Jerome Riga <jerome@woolandthegang.com>
2014-02-21 01:54:12 +0000

    moved spree controllers in frontend
    Former-commit-id: fde372f03d3759cb5a9e7e8a02266dff16e20fb6 [formerly 223428170a79693c294a504cae50e26d1bff4994 [formerly 6d03e97792f7605cb4e0ce1f456f032305d6e828]]
    Former-commit-id: 65112d01477344d8428b90bbd59aad56d45bc89f
    Former-commit-id: 5cd68150e5334e12c2e08e78b3c23a294d9ac33a [formerly 15c72f53c65616aeba06d7e0f765dff465fc7847]
    Former-commit-id: 3631301bbad71568ba17ca81b897b399b0b7f3ec

---
+++ frontend/app/controllers/spree/product_pages_controller.rb
@@ -0,0 +1,60 @@
+module Spree
+  class ProductPagesController < Spree::StoreController
+    require_feature :product_pages
+
+    PER_PAGE = 6
+    
+    respond_to :html
+
+    def show
+      respond_to do |format|
+        format.html { show_html }
+        format.js { show_js }
+      end
+    end
+
+    def show_html
+      outcome = Spree::ShowProductPageService.run(permalink: params[:id], tab: params[:tab], variant_id: params[:variant_id], request: request, currency: current_currency)
+      if outcome.success?
+        if url = outcome.result[:redirect_to]
+          redirect_to url
+        else
+          @product_page = outcome.result[:decorated_product_page]
+          @selected_variant = @product_page.selected_variant
+
+          @variants = render_variants(PER_PAGE ,@selected_variant)
+        end
+      else
+        Rails.logger.error( outcome.errors.message_list.join(', ') )
+        redirect_to spree.root_path
+      end
+    end
+
+    # This is used for the endless scrolling pagination we have put in place
+    # Todo put this into a service
+    def show_js
+
+      if params[:variant_id]
+        @selected_variant = Spree::Variant.find params[:variant_id]
+      end
+
+      @product_page = Spree::ProductPage.find_by_permalink(params[:id]).decorate( context:  {
+        tab:     params[:tab],
+        request: request,
+        current_currency: current_currency,
+        selected_variant: @selected_variant
+      } )
+
+      @variants = render_variants(PER_PAGE ,@selected_variant)
+    end
+
+    private
+
+    def render_variants( per_page, selected_variant )
+      @current_currency = current_currency
+      @context = { context: { target: @product_page.target, current_currency: @current_currency, request: request } }
+      @product_page.made_by_the_gang_variants(selected_variant).page(params[:page]).per( per_page )
+    end
+
+  end
+end
--- frontend/app/controllers/spree/products_controller.rb
+++ frontend/app/controllers/spree/products_controller.rb
@@ -1,15 +1,23 @@
 module Spree
   class ProductsController < Spree::StoreController
     before_filter :load_product, :only => :show
+    before_filter :load_selected_variant, :only => :show
+
+# Commented out until we get a response from pinterest about 
+# how they will deal with redirects
+    before_filter :redirect_to_product_pages, :only => :show
+    
+    before_filter :check_stock, :only => :show
     rescue_from ActiveRecord::RecordNotFound, :with => :product_not_found
     helper 'spree/taxons'
-
-
-
+    helper 'spree/products_ecom'
+    
     respond_to :html
 
     def index
-      @searcher = build_searcher(params)
+      @searcher = Config.searcher_class.new(params)
+      @searcher.current_user = try_spree_current_user
+      @searcher.current_currency = current_currency
       @products = @searcher.retrieve_products
     end
 
@@ -35,21 +43,47 @@ module Spree
     end
 
     private
-      def product_not_found
-        flash[:error] = "Product not found"
-        redirect_to root_url
+    def redirect_to_product_pages
+      if Flip.product_pages?
+        outcome = Spree::ProductPageRedirectionService.run(product: @product, variant: @selected_variant)
+        redirect_to outcome.result[:url], status: outcome.result[:http_code]
+      end
+    end
+
+    def check_stock
+      outcome = Spree::VariantStockControlService.run(selected_variant: @selected_variant)
+      if outcome.result[:redirect_to]
+        flash[:notice] = outcome.result[:message]
+        redirect_to outcome.result[:redirect_to]
+      end
+    end
+
+    def load_selected_variant
+      if params[:option_values].blank?
+        @selected_variant = @product.first_variant_or_master
+      else
+        selected_option_values = params[:option_values].split('/') rescue []
+        @selected_variant = Spree::Variant.options_by_product(@product, selected_option_values)
+        @selected_variant ||= @product.first_variant_or_master
       end
 
-      def accurate_title
-        @product ? @product.name : super
+      if !params[:option_values].blank? && (@selected_variant.blank? || @selected_variant.is_master)
+        flash[:error] = Spree.t(:unknown_selected_variant) + "  " + selected_option_values.join(', ')
+        redirect_to product_url(@product)
       end
+    end
 
-      def load_product
-        if try_spree_current_user.try(:has_spree_role?, "admin")
-          @product = Product.find_by_permalink!(params[:id])
-        else
-          @product = Product.active(current_currency).find_by_permalink!(params[:id])
-        end
-      end
+    def product_not_found
+      flash[:error] = "Product not found"
+      redirect_to root_url
+    end
+
+    def accurate_title
+      @product ? @product.name : super
+    end
+
+    def load_product
+      @product = Product.active(current_currency).find_by_permalink!(params[:id]|| params[:product_id])
+    end
   end
 end
