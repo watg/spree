885618ead840bf7bd880fa25930643bf7231eb02
David Dawson <ddawson@Davids-MacBook-Air.local>
2014-03-07 19:04:08 +0000

    Missing methods
    Former-commit-id: b72da27596fe26fb9a5ad867a4dc59fac54269a8 [formerly 97e8887be3555d80287de21c496caae3c09ab5dc [formerly 9751176f724582272f991c7f1a3af427b6c91d23]]
    Former-commit-id: 42a13eae5c68c5740d299d657a2e39c56ab79f8f
    Former-commit-id: f02b62535707bdeb3d01556e3472dca9dd716da5 [formerly 7ba7bf87aaa5080a67eae1c6ce3310c6475f0401]
    Former-commit-id: d9c822619c65300e21325a1133acca54455ddd7d

--- core/app/models/spree/assembly_definition.rb
+++ core/app/models/spree/assembly_definition.rb
@@ -4,13 +4,14 @@ class Spree::AssemblyDefinition < ActiveRecord::Base
 
   has_many :assembly_definition_variants, dependent: :delete_all
   has_many :variants, through: :assembly_definition_variants
+  has_many :option_values, through: :variants
 
   accepts_nested_attributes_for :variants 
 
   delegate_belongs_to :part, :name
 
   def variant_options_tree_for(target, current_currency)
-    variants.options_tree_for(target, current_currency)
+    variants.in_stock.options_tree_for(target, current_currency)
   end
 
 end
