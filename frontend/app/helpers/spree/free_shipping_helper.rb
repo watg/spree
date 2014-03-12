7076f3f76904c6233b802b8607fa5daf7887767d
Steve Edwards <thecolonyroom@Steves-MacBook-Air.local>
2014-03-12 13:02:41 +0000

    Splitting the shipping modals into partials. Adding Martin's conditional logic.
    Former-commit-id: 11c4fc9bddd70081ea67bc3a402c668c9e60db9c [formerly e109fe7b24135878f0bf480be54e659f988ca561 [formerly 0eac49210201f742ebcd5a50ed92143b2ff53dff]]
    Former-commit-id: d79ed299613ba6c2d0783ce83ffeca1feb15d348
    Former-commit-id: 25cc40b7c576ffd88ce1f5bfdbd1d1eda9c2b7c8 [formerly 58666bedae146a11978a8b8f610b213d7df9676c]
    Former-commit-id: aadcd754848f123a7add61a26b99b3d999bccf42

--- frontend/app/helpers/spree/free_shipping_helper.rb
+++ frontend/app/helpers/spree/free_shipping_helper.rb
@@ -2,13 +2,12 @@ module Spree
   module FreeShippingHelper
 
     FREE_SHIPPING_COUNTRY_CODES = {
-      'US' => '$175.00', 
-      'CA' => '$175.00', 
-      'UK' => '£95.00', 
-      'FR' => '€125.00', 
-      'SP' => '€65.00'
+      'US' => '$175.00',
+      'CA' => '$175.00',
+      'GB' => '£95.00',
+      'FR' => '€125.00',
+      'ES' => '€65.00'
     }
-    
 
     def free_shipping?
       FREE_SHIPPING_COUNTRY_CODES.keys.include? current_country_code
