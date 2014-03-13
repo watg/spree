2c653a14d905849a77082b05853ce5a95fcc3167
Martin Tomov <mtomov@outlook.com>
2014-03-13 17:00:43 +0000

    Removing the currency select box for everyone but admin users
    Former-commit-id: 0c48b0db2d1a7ef2603980ca3d0c577fb13a275f [formerly 6b01732ba4b55a32759f2a0d2ba3fefed6ed551c [formerly dd1458df4a0f707ed3fe4e18b8fe32281bd19409]]
    Former-commit-id: 5fdffed0545143bc06ab8d57a4ace4feaa1fe555
    Former-commit-id: 42be2f1825e712dd19b3b2b78d942d7d2ef914ca [formerly dc19a3f57aa1ece3063d311ebe0f7745124f1d1b]
    Former-commit-id: b7331993e38e18150d70caaf3bf7b3579a4e40d9

--- frontend/app/controllers/spree/currency_controller.rb
+++ frontend/app/controllers/spree/currency_controller.rb
@@ -2,7 +2,7 @@ module Spree
   class CurrencyController < Spree::StoreController
     def set
       currency = supported_currencies.find { |c| c.iso_code == params[:currency] }
-      session[:currency] = params[:currency] if Spree::Config[:allow_currency_change]
+      session[:currency] = params[:currency] if Spree::Config[:allow_currency_change] || (spree_current_user && spree_current_user.has_spree_role?("admin"))
       respond_to do |format|
         format.json { render :json => !currency.nil? }
         format.html do
