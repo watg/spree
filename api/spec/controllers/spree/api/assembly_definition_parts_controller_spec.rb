5f623d15065e488c7cc887bd7230ea8c88012f36
David Dawson <ddawson@Davids-MacBook-Air.local>
2014-03-18 13:09:43 +0000

    Fixed another spec
    Former-commit-id: bd55f14fc1062c4d95785cb6a83ac3b601acd21f [formerly 57a95afe5b32efbccb7fc357a2f562355ab3d556 [formerly 0c2d74d788283375fe240240cee7052d247473d8]]
    Former-commit-id: ae22e30b858b31baeaab0848fcebc81d8caab17e
    Former-commit-id: 40505e787e1b68d6cad94f39509e38d2147527ed [formerly 8e088f41f6742f165c2efc940326e22906c50707]
    Former-commit-id: fea63349cb70328a9b9fcaa2ffa0ba3b5cc55217

--- api/spec/controllers/spree/api/assembly_definition_parts_controller_spec.rb
+++ api/spec/controllers/spree/api/assembly_definition_parts_controller_spec.rb
@@ -2,7 +2,16 @@ require 'spec_helper'
 
 describe Spree::Api::AssemblyDefinitionPartsController do
   render_views
-  let!(:ass_def_part) { Spree::AssemblyDefinitionPart.create(assembly_definition_id: 12, count: 3, product_id: 3, optional: true) }
+
+
+  let(:variant_assembly) { create(:variant) }
+  let(:assembly_definition) { create(:assembly_definition, variant: variant_assembly) }
+
+  let(:variant_part)  { create(:base_variant) }
+  let(:product_part)  { variant_part.product }
+
+  let!(:ass_def_part) { Spree::AssemblyDefinitionPart.create(assembly_definition: assembly_definition, count: 3, product: product_part, optional: true) }
+
   let!(:variants) { [create(:variant)] }
   let!(:attributes) { [:id, :options_text] }
   before do
