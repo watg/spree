72cc1ef573bea635bde85db69490e783c9813ba6
Martin Tomov <mtomov@outlook.com>
2014-03-20 19:51:09 +0000

    Fixing the first batch of Order Populator specs
    Former-commit-id: 634840166d87db2719f646d6e6034d4781ba3b13 [formerly 89628311788e37b06c03f89110bc285359913703 [formerly a5b6d4a7d416acb23f2fdc1536d89bfbe56fdc9d]]
    Former-commit-id: 925cc25f82df6f6ff6a1efcb321de682ef1b3657
    Former-commit-id: 80e851e851161d13254a3a20def537a72d3ccce9 [formerly 7d1d2a9bdc1d6165f2120dbbe9118507712d7c0f]
    Former-commit-id: 100958766afb245d5669b29a32e1730e035ddf99

--- core/spec/models/spree/order_populator_spec.rb
+++ core/spec/models/spree/order_populator_spec.rb
@@ -1,28 +1,37 @@
 require 'spec_helper'
 
 describe Spree::OrderPopulator do
-  before { pending("decorator failure") }
 
   let(:order) { double('Order') }
   subject { Spree::OrderPopulator.new(order, "USD") }
 
-  context "with stubbed out find_variant" do
-    let(:variant) { double('Variant', :name => "T-Shirt", :options_text => "Size: M") }
+  context "without assembly_definition" do
+    let(:variant) { create(:variant, price: 60.00) }
+    let(:product) { variant.product }
+    let(:target_id) { 45 }
 
     before do
-     Spree::Variant.stub(:find).and_return(variant) 
-     order.should_receive(:contents).at_least(:once).and_return(Spree::OrderContents.new(self))
+      order.should_receive(:contents).at_least(:once).and_return(Spree::OrderContents.new(self))
     end
 
     context "with products parameters" do
       it "can take a list of products and add them to the order" do
-        order.contents.should_receive(:add).with(variant, 1, subject.currency).and_return double.as_null_object
-        subject.populate(:products => { 1 => 2 }, :quantity => 1)
+        order.contents.should_receive(:add).with(variant, 1, subject.currency, nil, [], [], target_id).and_return double.as_null_object
+        subject.populate(:products => { variant.product.id => variant.id }, :quantity => 1, :target_id => 45)
+      end
+
+      it "can take a list of products with options and add them to the order" do
+        part1 = create(:part)
+        part2 = create(:part)
+        product.add_part(part1, 1, true)
+        product.add_part(part2, 2, true)
+        order.contents.should_receive(:add).with(variant, 1, subject.currency, nil, [ [part2, 2], [part1, 1] ], [], target_id).and_return double.as_null_object
+        subject.populate(:products => { variant.product.id => variant.id, :options => [part1.id, part2.id] }, :quantity => 1, :target_id => 45)
       end
 
       it "does not add any products if a quantity is set to 0" do
         order.contents.should_not_receive(:add)
-        subject.populate(:products => { 1 => 2 }, :quantity => 0)
+        subject.populate(:products => { variant.product.id => variant.id }, :quantity => 0)
       end
 
       context "variant out of stock" do
@@ -33,7 +42,7 @@ describe Spree::OrderPopulator do
         end
 
         it "adds an error when trying to populate" do
-          subject.populate(:products => { 1 => 2 }, :quantity => 1)
+          subject.populate(:products => { variant.product.id => variant.id }, :quantity => 1)
           expect(subject).not_to be_valid
           expect(subject.errors.full_messages.join).to eql "error message"
         end
@@ -42,7 +51,7 @@ describe Spree::OrderPopulator do
       # Regression test for #2695
       it "restricts quantities to reasonable sizes (less than 2.1 billion, seriously)" do
         order.contents.should_not_receive(:add)
-        subject.populate(:products => { 1 => 2 }, :quantity => 2_147_483_648)
+        subject.populate(:products => { variant.product.id => variant.id }, :quantity => 2_147_483_648)
         subject.should_not be_valid
         output = "Please enter a reasonable quantity."
         subject.errors.full_messages.join("").should == output
@@ -51,9 +60,21 @@ describe Spree::OrderPopulator do
 
     context "with variant parameters" do
       it "can take a list of variants with quantites and add them to the order" do
-        order.contents.should_receive(:add).with(variant, 5, subject.currency).and_return double.as_null_object
-        subject.populate(:variants => { 2 => 5 })
+        order.contents.should_receive(:add).with(variant, 5, subject.currency, nil, [], nil, nil).and_return double.as_null_object
+        subject.populate(:variants => { variant.id => 5 })
       end
     end
+  end
+
+
+  context "with assembly definition" do
+    let(:variant) { create(:variant, price: 60.00) }
+    let(:product) { variant.product }
+    let(:target_id) { 45 }
+
+    before do
+      order.should_receive(:contents).at_least(:once).and_return(Spree::OrderContents.new(self))
+    end
+
   end
 end
