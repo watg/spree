feab94ddd0da4752a1a81d026e24d041eea69f82
Jerome Riga <jerome@woolandthegang.com>
2014-03-25 19:57:28 +0000

    linkshare pixel receipt with tax consideration
    Former-commit-id: 8928ca4341dd786d0ea66ee3ebc512cd53085a28 [formerly d38e6219dcc2d31e95d477caeae4ec8883a4c8d7 [formerly 74e2e10a8f52e0f156a6eadd66b146c901cd8957]]
    Former-commit-id: 7d9fc9cfcb8c7e58b9c52db1758e99ea7bbe5ef5
    Former-commit-id: e34ce6893ab33756e5e7c06c9f88e57716a83d44 [formerly f569f2ca963005c34b187d78d1fffe032960865c]
    Former-commit-id: 8eceb1faedfd2e0897d1c55c7e86c9c9ecfa44a4

--- core/spec/models/spree/line_item_spec.rb
+++ core/spec/models/spree/line_item_spec.rb
@@ -32,6 +32,38 @@ describe Spree::LineItem do
     end
   end
 
+  describe "#price_without_tax" do
+    context "order in USD" do
+      let(:order) { build(:order, currency: "GBP") }
+      before do
+        allow(order).to receive(:line_items).and_return([1])
+        subject.price = 45.99
+        subject.order = order
+      end
+      its(:price_without_tax) {should eq subject.price}
+    end
+    context "order in EUR" do
+      pending "Tax work"
+      let(:order) { build(:order, currency: "GBP") }
+      before do
+        allow(order).to receive(:line_items).and_return([1])
+        subject.price = 45.99
+        subject.order = order
+      end
+      #its(:price_without_tax) {should eq (subject.price * (1- Spree::Order::UK_EU_TAX_RATE))}
+    end
+    context "order in GBP" do
+      pending "Tax work"
+      let(:order) { build(:order, currency: "GBP") }
+      before do
+        allow(order).to receive(:line_items).and_return([1])
+        subject.price = 45.99
+        subject.order = order
+      end
+      #its(:price_without_tax) {should eq (subject.price * (1- Spree::Order::UK_EU_TAX_RATE))}
+    end
+  end
+
   context '#weight' do
     let(:variant10) { create(:variant, weight: 10) }
     let(:variant7)  { create(:variant, weight: 7) }
