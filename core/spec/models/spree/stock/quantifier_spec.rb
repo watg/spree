ed8b2e057b9ba34db07914248d2302d99bb7ef51
David Dawson <ddawson@Davids-MacBook-Air.local>
2014-03-26 17:09:26 +0000

    rmoeved the old assembly quanitifier tests
    Former-commit-id: 9a636672b6b5db734d2338822d8d5f8e64e4b7fc [formerly 13465e2bf7dedc2436eac7f5981c23df1787d675 [formerly 167b5c1622d469d30129c80f7a1bc7a270a60afb]]
    Former-commit-id: 18dcdd51fef631b8e7fb124597cdb7012886ffbe
    Former-commit-id: 9cb7a0297f91348d27c24a905cf7d88d5838c9e6 [formerly fa06feb383766c92d9877bfb3337ac82157c0ab3]
    Former-commit-id: 0ce2c4229e826a6bd4162018cd652ccf470bb917

--- core/spec/models/spree/stock/quantifier_spec.rb
+++ core/spec/models/spree/stock/quantifier_spec.rb
@@ -5,14 +5,6 @@ describe Spree::Stock::Quantifier do
   let(:kit) { create(:product, product_type: :kit) }
   let(:made_by_the_gang) { create(:product, product_type: :made_by_the_gang) }
 
-  it "returns Simple Quantifier for non-assembly variants" do
-    expect(subject.new(made_by_the_gang.master)).to be_kind_of(Spree::Stock::SimpleQuantifier)
-  end
-
-  it "returns Assembly Quantifier for kits and assembly variants" do
-    expect(subject.new(kit.master)).to be_kind_of(Spree::Stock::AssemblyQuantifier)
-  end
-
   describe "#can_supply_order?" do
     let(:order) { create(:order_with_line_items) }
 
