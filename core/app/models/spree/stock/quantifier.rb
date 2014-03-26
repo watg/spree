9b4b948518fbb36c79cdfb4f1b19fd331118bbd7
David Dawson <ddawson@Davids-MacBook-Air.local>
2014-03-26 16:05:32 +0000

    More required_parts code... adios amigos
    Former-commit-id: 256f64a72e1821c33fa1e2f2232ccdaad2178ebb [formerly 3bb9aade475f0929362deb956da217789cb79806 [formerly f9fcee6cc883c418f2f196e154ceed34f4e0c824]]
    Former-commit-id: b8f9b78fe562e4bf5f74fc211c5138311a3d587d
    Former-commit-id: 08770ff9c42899429392bc854b53e96180bc74a3 [formerly a49407717e465cabf75237cad3857d310a4194c6]
    Former-commit-id: b3bc989e28ff2e88a3342b6c1815e1f5f09dee31

--- core/app/models/spree/stock/quantifier.rb
+++ core/app/models/spree/stock/quantifier.rb
@@ -1,43 +1,56 @@
 module Spree
   module Stock
     class Quantifier
+      attr_reader :stock_items
 
-      class << self
-        def new(variant)
-          instance = (variant.kind_of?(Spree::Variant) ? variant : Spree::Variant.find(variant))
-          klass_name = (has_parts?(instance) ? 'AssemblyQuantifier' : 'SimpleQuantifier')
-          klass = "Spree::Stock::#{klass_name}".constantize
+      def initialize(variant)
+        @variant = resolve_variant_id(variant)
+        @stock_items = Spree::StockItem.joins(:stock_location).where(:variant_id => @variant, Spree::StockLocation.table_name =>{ :active => true})
+      end
 
-          klass.new(instance)
-        end
-
-        def has_parts?(variant)
-          variant.product.can_have_parts?
-        end
-
-        def can_supply_order?(order, desired_line_item=nil)
-          record = lambda {|line_item| {variant_id: line_item.variant_id, quantity: line_item.quantity}}
-          line_item_to_record = lambda {|li, lio|([li] + [lio]).flatten.compact.map(&record) }
-
-          a = line_item_to_record[order.line_items,  order.line_items.map(&:line_item_options).flatten]
-          b = line_item_to_record[desired_line_item, (desired_line_item ? desired_line_item.line_item_options : [])]
-
-          variant_quantity_grouping = (a + b).reduce({}) {|hsh, c|
-                             k = c[:variant_id]
-                             hsh[k] ||= 0; hsh[k] += c[:quantity]
-                             hsh}
-          errors = []
-          stock_check = variant_quantity_grouping.map {|variant_id, quantity|
-                            variant = Spree::Variant.find(variant_id)
-                            in_stock = Spree::Stock::Quantifier.new(variant).can_supply?(quantity)
-                            errors << Spree.t(:out_of_stock, :scope => :order_populator, :item => %Q{#{variant.name} #{variant.options_text}}.inspect) unless in_stock
-                            in_stock}
-          result = stock_check.reduce(true) {|can_supply,c| can_supply && c}
-
-          {in_stock: result, errors: errors}
+      def total_on_hand
+        if Spree::Config.track_inventory_levels
+          stock_items.sum(:count_on_hand)
+        else
+          Float::INFINITY
         end
       end
 
+      def backorderable?
+        stock_items.any?(&:backorderable)
+      end
+
+      def can_supply?(required = 1)
+        total_on_hand >= required || backorderable?
+      end
+
+      # return variant when passed either variant object or variant id
+      def resolve_variant_id(variant)
+        variant = Spree::Variant.find_by_id(variant) unless variant.respond_to?(:should_track_inventory?)
+        variant
+      end
+
+      def can_supply_order?(order, desired_line_item=nil)
+        record = lambda {|line_item| {variant_id: line_item.variant_id, quantity: line_item.quantity}}
+        line_item_to_record = lambda {|li, lio|([li] + [lio]).flatten.compact.map(&record) }
+
+        a = line_item_to_record[order.line_items,  order.line_items.map(&:line_item_options).flatten]
+        b = line_item_to_record[desired_line_item, (desired_line_item ? desired_line_item.line_item_options : [])]
+
+        variant_quantity_grouping = (a + b).reduce({}) {|hsh, c|
+          k = c[:variant_id]
+          hsh[k] ||= 0; hsh[k] += c[:quantity]
+          hsh}
+        errors = []
+        stock_check = variant_quantity_grouping.map {|variant_id, quantity|
+          variant = Spree::Variant.find(variant_id)
+          in_stock = Spree::Stock::Quantifier.new(variant).can_supply?(quantity)
+          errors << Spree.t(:out_of_stock, :scope => :order_populator, :item => %Q{#{variant.name} #{variant.options_text}}.inspect) unless in_stock
+          in_stock}
+        result = stock_check.reduce(true) {|can_supply,c| can_supply && c}
+
+        {in_stock: result, errors: errors}
+      end
     end
   end
 end
--- frontend/spec/controllers/spree/products_controller_spec.rb
+++ frontend/spec/controllers/spree/products_controller_spec.rb
@@ -79,10 +79,6 @@ describe Spree::ProductsController do
   		subject.stub :authorize! => true, :ensure_api_key => true
   		subject.stub(:current_order).and_return(order)
   		subject.stub(:set_current_order).and_return(order)
-  		Spree::Product.stub :find_by_permalink! => product
-  		allow(Spree::VariantStockControlService).to receive(:run).with(selected_variant: variant) { 
-  		  OpenStruct.new(result: {redirect_to: nil} )
-  		}
 	  end
 
 	  it "should find selected variant from params option_values" do
@@ -90,20 +86,6 @@ describe Spree::ProductsController do
   		spree_get :show, params
   		expect(response.status).to eq(200)
 	  end
-
-	  context "stock level" do
-  		before do
-  		  allow(Spree::Variant).to receive(:options_by_product) { variant }
-  		  allow(Spree::VariantStockControlService).to receive(:run).with(selected_variant: variant) { 
-  		    OpenStruct.new(result: {redirect_to: '/products/other/blue-large', message: 'selected_variant no in stock. redirected to other product'} )
-  		  }
-  		end
-  		it "redirects to a variant in stock" do
-  		  spree_get :show, params
-  		  expect(response.status).to eq(302) 
-  		end
-	  end
-
 
 	  context "Product Pages active" do
   		before do
@@ -120,7 +102,6 @@ describe Spree::ProductsController do
 	  end
 
   end
-
 
 
 end
