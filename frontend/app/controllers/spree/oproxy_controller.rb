b94688f539de284ff3f5bca9121a8c98700c4914
Jerome Riga <jerome@woolandthegang.com>
2014-04-02 16:20:14 +0100

    pass all params to olapic api
    Former-commit-id: 48f2f0c626b98ab6483894bdbc2a0aaa1b79cb5d [formerly b5be8eaed00d0d845c28d40e03832008f9aa00d9 [formerly 209b06af444eec57e4faba065a3dc19431c00db4]]
    Former-commit-id: 45ad181316d03e2a1baff9cc7c724043d9398195
    Former-commit-id: adab5526b6b77d9946bac8a3cf76ed836406a7f3 [formerly 782fb001afed836a8a0a2254394921f7bfb67719]
    Former-commit-id: 2abf310481d10c87130eea2f26fe729bd86c0ad1

--- frontend/app/controllers/spree/oproxy_controller.rb
+++ frontend/app/controllers/spree/oproxy_controller.rb
@@ -3,7 +3,15 @@ module Spree
     include Olapic::Stream
     protect_from_forgery
     def show
-      render text: olapic_data(params[:url])
+      render text: olapic_data(params[:url], api_params)
+    end
+
+    private
+    def api_params
+      hsh = params.dup
+      hsh.delete(:action)
+      hsh.delete(:controller)
+      hsh
     end
   end
 end
