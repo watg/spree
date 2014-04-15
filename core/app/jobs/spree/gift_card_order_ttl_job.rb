f6337416ce2ea792b5aff218d3dfdc4f98b0fb6b
Jerome Riga <jerome@woolandthegang.com>
2014-04-15 12:40:56 +0100

    gift card core feature
    Former-commit-id: a7088d72ca7d36371a8b9ce464006dd1d8625076 [formerly 5ccf16fafd23eba3507d7424d5c3da71ee234543 [formerly 949ce1f33b9b0f49cffc05a229c6d119eb0fabd9]]
    Former-commit-id: 1c977bfb00d33d2ca6779e34d48647a33da609e0
    Former-commit-id: 1832f5b93d469dd41c579aa3ca4ab95f0078ab64 [formerly 7c0cd3f5626f2dc4b565910d84b38e06f1c2a46c]
    Former-commit-id: f244ee5beed448f17d02534a4dfe0d603c8c617f

--- core/app/jobs/spree/gift_card_order_ttl_job.rb
+++ core/app/jobs/spree/gift_card_order_ttl_job.rb
@@ -1,16 +1,14 @@
 module Spree
   GiftCardOrderTTLJob = Struct.new(:order, :gift_card) do
-    # TODO: [gift-card] this blows up because originator_type/id are
-    # called source now
     def perform
       if not self.order.payments.completed.any?
         self.gift_card.reactivate
 
-        adjustment = self.order.adjustments.gift_card.where(originator_id: self.gift_card.id).first
+        adjustment = self.order.adjustments.gift_card.where(source_id: self.gift_card.id).first
         if adjustment
           adjustment.amount = 0
           adjustment.label = "[VOID] NOT USED WITHIN ALLOWED TIME -- #{adjustment.label}"
-          adjustment.finalize!
+          adjustment.close!
         end
       end
     end
