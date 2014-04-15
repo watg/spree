54f460813fc0d247cc5ee815bdae44503bc9f70d
Jerome Riga <jerome@woolandthegang.com>
2014-04-15 13:02:54 +0100

    fixed gift card usage service spec
    Former-commit-id: 4ff4d98b098ad7d02fdaabce38815b888f6445e5 [formerly 5dccd04ea6a3b7745f71ed7920ce302903328c09 [formerly 991088641d72c77fa741586af82af52dec89b41c]]
    Former-commit-id: d803b6fa967dca78c9d6d9b489a4d429ce05dd53
    Former-commit-id: 973bffd4cf8e2ad6f89fc6f92f6736c584f4708d [formerly 1a1bf695dd64c893ecc6090f97a1b09c169df4d4]
    Former-commit-id: b475efd9025d364586862d24c36a336df35bf523

--- backend/spec/services/use_gift_card_service_spec.rb
+++ backend/spec/services/use_gift_card_service_spec.rb
@@ -5,15 +5,13 @@ describe Spree::UseGiftCardService do
   let(:order) { create(:order, currency: 'GBP') }
   let(:gift_card) { create(:gift_card, currency: 'GBP') }
 	
-pending('THE WHOLE OF GIFT CARD HAS TO BE REVIEWED CLOSELY -- ')
-
-  xit "allows customer to use gift card" do
+  it "allows customer to use gift card" do
     outcome = subject.run(order: order, code: gift_card.code)
     expect(outcome).to be_success
-    expect(order.reload.adjustments.gift_card.first.originator).to eq(gift_card)
+    expect(order.reload.adjustments.gift_card.first.source).to eq(gift_card)
   end
 
-  xit "adds future job after updating order" do
+  it "adds future job after updating order" do
     mock_job = double('job')
     
     expect(Spree::GiftCardOrderTTLJob).to receive(:new)
@@ -22,12 +20,12 @@ pending('THE WHOLE OF GIFT CARD HAS TO BE REVIEWED CLOSELY -- ')
 
   context "validation" do
     let(:order_in_usd) { build(:order, currency: 'USD') }
-    xit "checks that order and gift card currency are the same" do
+    it "checks that order and gift card currency are the same" do
       outcome = subject.run(order: order_in_usd, code: gift_card.code)
       expect(outcome.errors.keys).to include('wrong_currency')
     end
 
-    xit "checks expiry_date" do
+    it "checks expiry_date" do
       allow(gift_card).to receive(:expiry_date) { 1.day.ago }
       outcome = subject.run(order: order_in_usd, code: gift_card.code)
       expect(outcome.errors.keys).to include('wrong_currency')
