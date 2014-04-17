3fec371e0158956d7708796fc12fc05a81e30a12
Jerome Riga <jerome@woolandthegang.com>
2014-04-17 18:05:55 +0100

    TODO: script to renames s3 folders product_pages => product_page_images product_page_tabs => product_page_tab_images index_page_items => index_page_item_images products => images
    TODO: option value
    move image out of products to option_values folder
    
    Former-commit-id: 1cf66fcdd6d2de6d69712e761864075265cadeed [formerly 6e973d8a41faf4dbb457585a9c089708f5471258 [formerly da4f5f6915c9af19411d86f8f8e8219e4c1ed3e1]]
    Former-commit-id: ee16256e0f7a1276dd3a0f5f19f8c4abaaf4ca00
    Former-commit-id: 698d34bf0f79d62c5c60d32a74b73cb918a685bd [formerly 55a12566ba8fa8c3148b99bd4f7846c9916e83ec]
    Former-commit-id: 8dd07692a049f1613ba0b6ae37d54587da2d6a4f

--- core/app/models/spree/index_page_item_image.rb
+++ core/app/models/spree/index_page_item_image.rb
@@ -4,8 +4,6 @@ module Spree
     has_attached_file :attachment,
       :styles        => { large: "640x900>", small: '310x396>', mini: '150x150>' },
       :default_style => :large,
-      :url           => "/spree/index_page_items/:id/:style/:basename.:extension",
-      :path          => ":rails_root/public/spree/index_page_items/:id/:style/:basename.:extension",
       :convert_options =>  { :all => '-strip -auto-orient' },
       :keep_old_files => false
 
