1aa16c23724e1f185816c79639bff36ecb404472
David Dawson <david.dawson@gmail.com>
2014-04-18 09:32:00 +0100

    moved logic for subcribe from spree_auth_devise to spree_chimpy
    Former-commit-id: d36ab50552c85bb43baabd47afd4bea7411c0c46 [formerly 7ebda99819ce7c3df88f686053067e579b86d43c [formerly ab2bef79d537ee2076d2299bb7e5e00506d17afc]]
    Former-commit-id: 4b1c67c26323f578ec77ecd6d297650b88a95e14
    Former-commit-id: ec07b663c18ce9ed8d5dc76409484417bbe136d0 [formerly 96c0f479abbac66abe500a0590db12e26d0d3697]
    Former-commit-id: 8b41fccf1423e8e02d8c87c551c0746014421b31

--- core/lib/spree/permitted_attributes.rb
+++ core/lib/spree/permitted_attributes.rb
@@ -90,7 +90,7 @@ module Spree
       :meta_description, :meta_keywords, :meta_title, :child_index]
 
     # TODO Should probably use something like Spree.user_class.attributes
-    @@user_attributes = [:email, :password, :password_confirmation]
+    @@user_attributes = [:email, :password, :password_confirmation, :try_subscribe]
 
     @@variant_attributes = [
       :name, :presentation, :cost_price, :lock_version,
