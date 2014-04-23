3edc03430de9dfb8a7fbbda50a70e9b017b29759
Jerome Riga <jerome@woolandthegang.com>
2014-04-23 18:05:51 +0100

    added back missing image helpers
    Former-commit-id: 3eb7e68b4d6b85e88ab1c0f7ae6d08bff7e218c7 [formerly c2f42ba86f2ae7513070efbdb52699011caa6989 [formerly 45f087b4ec7fa1ccae499dfbb78cc1f2cea48ce8]]
    Former-commit-id: d98254b57e5372a825593dcc34b665e8ac6cf027
    Former-commit-id: 7b111e2fe4d9c5954a72880c17e9eaade2988cc0 [formerly 564cead3dd74da84e9b251f3fdd47c2420466b96]
    Former-commit-id: 73072a425f9914a0e832354766b6761e618ad88f

--- backend/app/helpers/spree/admin/images_helper.rb
+++ backend/app/helpers/spree/admin/images_helper.rb
@@ -1,6 +1,23 @@
 module Spree
   module Admin
     module ImagesHelper
+
+      def get_variant_id(image)
+        if image.viewable_type == 'Spree::Variant'
+          image.viewable_id
+        elsif image.viewable_type == 'Spree::VariantTarget'
+          image.variant_id
+        end
+      end
+
+      def get_personalisation_id(image)
+        if image.viewable_type == 'Spree::Personalisation'
+          image.viewable_id
+        else
+          nil
+        end
+      end
+
       def options_text_for(image)
         if image.viewable.is_a?(Spree::Variant)
           if image.viewable.is_master?
@@ -15,4 +32,3 @@ module Spree
     end
   end
 end
-
