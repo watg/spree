66ff612101813cb5fc8fd3c5edb4d66a9015863a
David Dawson <david.dawson@gmail.com>
2014-05-02 17:23:59 +0100

    Ensure the pinterset cherry-pick works
    Former-commit-id: ff2e060c8e6b7573e1fd1a5d97d305573e741f8c [formerly fc35673df7d95a010feea6fade2934c8c1654534 [formerly 3af93491b94e2186fef93f5f4c0767c1ec7edb0e]]
    Former-commit-id: fb9dabc795a1ffce4c32f0f2317f2dc383adb3cf
    Former-commit-id: fda94da9a5e6b5264ee5c8c1943392953212a83e [formerly 4b44a6738336773b1fc4b47f3fd5bef9a33d937a]
    Former-commit-id: 70e07e297a4c4ae941af282c87018b333975b2f9

--- core/app/models/spree/martin_product_type.rb
+++ core/app/models/spree/martin_product_type.rb
@@ -1,14 +1,16 @@
 module Spree
   class MartinProductType < ActiveRecord::Base
-    validates_inclusion_of :category, :in => %w(rtw assembly supply gift_card packaging)
+    validates_inclusion_of :category, :in => %w(rtw kit assembly supply gift_card packaging)
 
     def nature
       digital? ? :digital : :physical
     end
 
+    # This should really be kit? and the categoty should be kit
     def assembly?
-      category == 'assembly'
+      category == 'assembly' || category == 'kit'
     end
+    alias_method :kit?, :assembly?
     
     def rtw?
       category == 'rtw'
