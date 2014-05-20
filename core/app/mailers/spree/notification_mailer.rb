51ae56b544d0e198a23a6aa90fdcd855a39b917f
Jerome Riga <jerome@woolandthegang.com>
2014-05-20 16:31:55 +0100

    notification email work with all sort of input params
    Former-commit-id: a4191b87d1810289890a352e46e7aef1ecaa26ae [formerly a88ce07f609dedc2eb5d3878e5aba6f931aa2b13 [formerly d98050dfef0ddef2ab06a689929f57d85a026961]]
    Former-commit-id: 2cf5a314c156aa1759c4911b1b48ea1e3fccc515
    Former-commit-id: bbfb038d5fb4db2e2b5ac6554857a8c226ca9d3f [formerly 82efd96582638b543f2a5229320e8be6a2962b29]
    Former-commit-id: 73f93bdf48e030e3d6f21e7aa56972f043688c08

--- core/app/mailers/spree/notification_mailer.rb
+++ core/app/mailers/spree/notification_mailer.rb
@@ -5,11 +5,11 @@ module Spree
     def send_notification(message, to=nil, subject=nil)
       to ||= 'techadmin@woolandthegang.com'
       subject ||= 'Notification from the site'
-      @message = message
+      @message = message.inspect
       mail(:to => to, :subject => subject)
 
       mandrill_default_headers(template: "admin_notifications")
-      headers['X-MC-MergeVars'] = { message: message.gsub(/\n/, '<br>') }.to_json
+      headers['X-MC-MergeVars'] = { message: @message.gsub(/\n/, '<br>') }.to_json
     end
 
   end
