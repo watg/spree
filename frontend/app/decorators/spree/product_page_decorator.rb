0a1eabe700360d4188bd4bae9f883a30ee0cfea3
Martin Tomov <mtomov@outlook.com>
2014-09-02 17:45:38 +0100

    Product Page Show refactored to use a cart partial. The Cart partial takes care of assembled and kit products.
    Former-commit-id: abaa8be5a0c94b8b194976b6cf864ce23e1b7a02 [formerly cd9fd76601848e3053e6efd1285810034b5aed04 [formerly 62aa5cd9658aa89e0e5639359953cb75c0aab96f]]
    Former-commit-id: 30b10eda3a55c4f8045b8721b392e7a00121113c
    Former-commit-id: dcab2b338159c6df715fec3f9a3d5466a96f341f [formerly f01b149ceb01d421f0caab479f43301689032d07]
    Former-commit-id: e1a441848c82be1e915c0c28fad3ac837ab1ad2b

--- frontend/app/decorators/spree/product_page_decorator.rb
+++ frontend/app/decorators/spree/product_page_decorator.rb
@@ -55,43 +55,20 @@ class Spree::ProductPageDecorator < Draper::Decorator
     selected_tab && selected_tab.made_by_the_gang? ? 'active' : ''
   end
 
-  def decorated_first_knit_your_own_product_variant
-    variant = nil
-    # TODO: kil this once the new kit functionality is live
-    # should be just
-    # variant = knit_your_own_product.master
-    if knit_your_own_product.assembly_definition
-      variant = knit_your_own_product.master
-    else
-      variant = knit_your_own_product.variants_for(object.target).first
-    end
-    if variant
-      variant.decorate(context: { current_currency: current_currency, target: object.target})
-    end
-  end
-
-  def decorated_knit_your_own_product
-    knit_your_own_product.decorate(context: {
-                           current_currency: current_currency,
-                           target: object.target })
-  end
-
   def knit_your_own_active_class
     selected_tab && selected_tab.knit_your_own? ? 'active' : ''
   end
 
-  def kit_description
-    description = object.knit_your_own.product.description_for(object.target).to_s rescue Spree.t(:product_has_no_description)
-    description = description.gsub(/(.*?)\r?\n\r?\n/m, '\1<br><br>')
-    description.gsub(/_/, '').gsub(/-/, '&ndash;').html_safe
-  end
-
-  def kit_name
-    object.knit_your_own.product.name
+  def active_tab(tab)
+    (selected_tab == tab) ? 'active' : ''
   end
 
   def knit_your_own_product
     object.knit_your_own.product
+  end
+
+  def made_by_the_gang_product
+    object.made_by_the_gang.product
   end
 
   def knit_your_own_product?
