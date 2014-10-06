440894a63ccf26b363af9c38f0381c5bb282a9e8
Martin Tomov <mtomov@outlook.com>
2014-10-06 14:21:14 +0100

    Performance Optimisations. Preload #line_item_parts and available stock_items.
    Former-commit-id: 09ce4646336f3c6919661af8f5534573c015054c [formerly c8934ae434775e2b2a590b59acd02b24a5f487ad [formerly 89c425ec7c13b230c2b9c4dfb7710240d0b68b63]]
    Former-commit-id: 88ad523699de3ae8af5743932d844d1e6a9f62af
    Former-commit-id: 3ec463ed566ac67b63dd401715c30a949afd94be [formerly 9e58a4c16490919537d3c0fae471016b9076de48]
    Former-commit-id: 2099fb2349634dda1e906ff2f3658437101826a7

--- core/app/models/spree/stock/availability_validator.rb
+++ core/app/models/spree/stock/availability_validator.rb
@@ -37,22 +37,28 @@ module Spree
 
 
       def validate_order(order)
+        ## Needs to be upgraded to the new syntax in rails 4.1
+        ## ActiveRecord::Associations::Preloader.new.preload(order, [line_items: :line_item_parts]).run
+        ActiveRecord::Associations::Preloader.new(order, [line_items: :line_item_parts]).run
+
         item_builder = Spree::Stock::OrderItemBuilder.new(order)
 
         # group the order's variants. Ex: {324=>3, 1405=>4, 321=>2, 323=>3, 322=>3}
         grouped_variants = item_builder.group_variants
 
-        # remove variant ids, which have allocated inventory units
-        # {324=>2, 1405=>3, 321=>2}
+        # remove variant ids, which have allocated inventory units. Ex: {324=>2, 1405=>3, 321=>2}
         grouped_units = order.inventory_units.where(pending: false).group(:variant_id).count
 
         grouped_variants = grouped_variants.each do |variant_id, count|
           grouped_variants[variant_id] = grouped_variants[variant_id].to_i - grouped_units[variant_id].to_i
         end.select {|v_id, count| count > 0}
 
+        preloaded_variants = Spree::Variant.where(id: grouped_variants.keys).includes(:stock_items).joins(stock_items: :stock_location).merge(Spree::StockLocation.active).references(:stock_location).load
+
         valids = grouped_variants.map do |variant_id, quantity|
-          variant = Spree::Variant.find(variant_id)
-          if !Stock::Quantifier.new(variant).can_supply? quantity
+          variant = preloaded_variants.find { |variant| variant.id == variant_id }
+
+          if !Stock::Quantifier.new(variant, variant.stock_items).can_supply? quantity
             display_name = %Q{#{variant.name}}
             display_name += %Q{ (#{variant.options_text})} unless variant.options_text.blank?
 
