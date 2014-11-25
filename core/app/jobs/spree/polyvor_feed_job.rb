333bccb6338910d73d961d63b4e642edd72f5787
Martin Tomov <mtomov@outlook.com>
2014-11-25 13:42:08 +0000

    Convert Polyvor to generate data from suites. Refactor Polyvor to use BaseFeedJob
    Former-commit-id: a2526c632e151a0192393e69c8e735ed130ec968 [formerly 2523aa690ddce2341a5cc6a86bcd22b8161ab47b [formerly 3c19bc6c31951088100b6c20535c70d13110e327]]
    Former-commit-id: 41ec8ae39591df62c66c4c3fdb642743b36e46b7
    Former-commit-id: a4145aeb2eb0717b98caf9feedb1c13613572949 [formerly 95cc32cc3adc076d3efdbb48ce2cc659c5803b92]
    Former-commit-id: 3f6fbe383001e56f5fbd8c0dd6c52d8da5e6c271

--- core/app/jobs/spree/polyvor_feed_job.rb
+++ core/app/jobs/spree/polyvor_feed_job.rb
@@ -1,8 +1,5 @@
 module Spree
-  class PolyvorFeedJob
-    include ActionView::Helpers
-    include ApplicationHelper
-
+  class PolyvorFeedJob < BaseFeedJob
     NAME = 'polyvor'
 
     CURRENCY = 'USD'
@@ -19,147 +16,54 @@ module Spree
         'color',
         'sizes',
         'tags',
+        'target',
         'category',
         'cpc_labels'
       ]
     CATEGORY = 'Clothing'
-    BRAND = 'Wool and the Gang'
-
-    # TODO: what about out of stock products?
-    # TODO this needs to be implemented
-    # TODO: we need to set this up to be hosted on S3 like the linkshare job
-    # TODO: it needs to refreshed once a day
     DEFAULT_IMAGE_URL = nil
 
-    def initialize
+    def feed
+      CSV.generate(col_sep: "\t") do |csv|
+        csv << header
 
+        all_variants do |suite, tab, product, variant|
+          csv << format_csv(suite, tab, product, variant)
+        end
+      end
     end
+
+
+  private
 
     def header
       HEADER
     end
 
-    def perform
-      persist( feed, config[:storage_method].to_sym)
-    end
-
-    private
-
-    def config
-      FEEDS_CONFIG[NAME].symbolize_keys
-    end
-
-    def persist(value, storage_method=:s3)
-      notify("storage_method : #{storage_method} not supported") unless [:cache, :s3].include?(storage_method)
-      if storage_method == :s3
-        storage_s3(value)
-      else
-        storage_cache(value)
-      end
-    end
-
-    def storage_cache(value)
-      path = File.join(Rails.root,'public', config[:name])
-      unless open(path, 'w') { |f| f.write(value); f.flush }
-        notify("#{NAME} feed could no be saved in CACHE path #{path}: #{value[0..100]}...")
-      end
-    end
-
-    def storage_s3(value)
-      s3_connection.buckets[ config[:s3_bucket]].
-        objects[  config[:name]     ].
-        write(value, :acl => :public_read)
-    rescue
-      notify("#{NAME} feed could no be saved on S3: #{value[0..400]}...")
-    end
-
-    def s3_connection
-      AWS::S3.new(
-        access_key_id:      ENV['AWS_ACCESS_KEY_ID'],
-        secret_access_key:  ENV['AWS_SECRET_ACCESS_KEY'],
-        s3_endpoint:        ENV['AWS_S3_ENDPOINT'],
-      )
-    end
-
-    def notify(alert_message)
-      # Sends an email to Techadmin
-      Spree::NotificationMailer.delay.send_notification(alert_message, Rails.application.config.tech_email_list)
-    end
-
-    def feed
-      CSV.generate(col_sep: "\t") do |csv|
-        csv << header
-        all_product_pages = Spree::ProductPage.joins(tabs: [:product]).includes(tabs: [ product: [ :variants ]])
-        all_product_pages.each do |product_page|
-          target = product_page.target
-          product_page.tabs.each do |tab|
-            if product = tab.product
-              if product.product_type.is_assembly?
-                variant = product.master
-                csv << format_csv(product_page, tab, product, variant)
-              else
-                product.variants.each do |variant|
-                  csv << format_csv(product_page, tab, product, variant)
-                end
-              end
-            end
-          end
-        end
-      end
-    end
-
-    def format_csv(product_page, tab, product, variant)
+    def format_csv(suite, tab, product, variant)
       [
-        product_page.name, # title
+        suite.name, # title
         BRAND, # brand
-        find_product_page_url(product_page, tab, variant), # url
+        variant_url(suite, tab, variant),
         nil, # cpc_tracking_url
-        variant_image_url(variant, product_page, tab), # imgurl
+        variant_image_url(variant, suite, tab),
         variant.price_normal_in(CURRENCY).amount, # price
-        sale_price(variant), # sale_price
+        current_price(variant), # normal or sale price when in_sale?
         CURRENCY,
-        product.clean_description_for(product_page.target), # description
-        safe_colour(variant), # color
+        product.clean_description_for(suite.target), # description
+        colour(variant), # color
         nil, # sizes
         nil, # tags
-        target(product_page), # target
+        gender(suite), # target
         CATEGORY, # category
         nil, # cpc_labels
       ]
     end
 
-    def target(product_page)
-      product_page.target ? product_page.target.name.humanize : 'Unknown'
-    end
 
-    def sale_price(variant)
-      price = variant.price_normal_sale_in(CURRENCY)
-      price.sale? ? price.amount : nil
-    end
-
-    def safe_colour(variant)
-      option_value = variant.option_values.detect{ |ov| ov.option_type.is_color? }
-      option_value.present? ?  option_value.presentation : nil
-    end
-
-    def route
-      Spree::Core::Engine.routes.url_helpers
-    end
-
-    def find_product_page_url(product_page, tab, variant)
-      v = variant.is_master ? nil : variant #does this line do anything important???
-      route.product_page_url(product_page, tab.url_safe_tab_type, v)
-    end
-
-    def variant_image_url(variant, product_page, tab)
-      target = product_page.target
-      image = variant.images_for(target).first
-      image = image ? image : variant.product.images_for(target).first
-      image = image ? image : tab.image
-      image ? image.attachment.url(:product) : DEFAULT_IMAGE_URL
+    def gender(suite)
+      suite.target ? suite.target.name.humanize : 'Unisex'
     end
 
   end
-
-
 end
--- core/spec/jobs/polyvor_feed_job_spec.rb
+++ core/spec/jobs/polyvor_feed_job_spec.rb
@@ -1,8 +1,18 @@
 require 'spec_helper'
 
+# There is a duplication of tests with the Base Feed Job Spec,
+# but keep to prevent regression.
+
 describe Spree::PolyvorFeedJob do
 
-context
+  let(:currency) { 'USD' }
+  let(:target) { build(:target, name: 'Women' ) }
+  let(:suite) { build(:suite, target: target) }
+  let(:tab) { Spree::SuiteTab.new(suite: suite) }
+
+  let(:product) { Spree::Product.new }
+  let(:variant) { Spree::Variant.new(id: 12, product: product) }
+
   describe '#header' do
     it "contains the header array" do
       expect(subject.send(:header)).to be_kind_of(Array)
@@ -11,154 +21,98 @@ context
 
   describe '#feed' do
     it 'generates a csv file' do
-      mock_csv = []
-      expect(CSV).to receive(:generate).and_return(mock_csv)
-      subject.send(:feed)
+      parsed_csv = CSV.parse(subject.feed, col_sep: "\t")
+
+      expect(parsed_csv).to be_kind_of Array
+      expect(parsed_csv.size).to eq 1
+      expect(parsed_csv[0]).to eq subject.send(:header)
     end
   end
 
   describe '#format_csv' do
-    before do
-      subject.stub(:find_product_page_url => "variant-url.com")
-      product_page.target.name = 'Female'
-      product_page.name = 'Tala Tank'
-    end
 
-    let(:tab) { create(:product_page_tab) }
-    let(:target) { create(:target, name: 'female' ) }
-    let(:product_page) { tab.product_page }
-    let(:product) {create(:product_with_variants, number_of_variants: 1)}
-    let(:variant) { product.variants.first }
+    before do
+      variant.price_normal_in(currency).amount = 8.99
+
+      suite.target.name = 'Women'
+      suite.name = 'Tala Tank'
+    end
 
     it 'creates a a correctly formatted file' do
-      expected_csv = ["Tala Tank",
+      expected_csv = [
+        "Tala Tank",
         "Wool and the Gang",
-        "variant-url.com",
+        "http://www.example.com/product/#{suite.permalink}",
         nil,
-        nil,
-        BigDecimal.new('0.00'),
-        nil,
+        nil, #image url
+        BigDecimal.new('8.99'),
+        8.99,
         "USD",
         "",
-        "Hot Pink", nil, nil,
-        "Female",
+        nil, #colour
+        nil,
+        nil,
+        "Women",
         "Clothing",
-        nil]
+        nil
+      ]
 
-      expect(subject.send(:format_csv, product_page, tab, product, variant)).to eq expected_csv
+      formatted_csv = subject.send(:format_csv, suite, tab, product, variant)
+      expect(formatted_csv).to eq expected_csv
+      expect(formatted_csv.size).to eq subject.send(:header).size
     end
   end
 
-  describe "#notification" do
-    before { Delayed::Worker.delay_jobs = false }
-    after { Delayed::Worker.delay_jobs = true }
-
-    it 'sends a notification if a product cannot be added to feed' do
-      expect {
-        subject.send(:notify, 'oh no!')
-      }.to change { ActionMailer::Base.deliveries.size }.by(1)
-    end
-  end
-
-  describe "#subject" do
-    let(:target) {create(:target, name: 'aliens')}
-    let(:product_page) {create(:product_page, target: target)}
-    let(:no_target) {create(:product_page, target: nil)}
-
-    it "extracts target or returns 'unknown'" do
-      expect(subject.send(:target, product_page)).to eq 'Aliens'
-      expect(subject.send(:target, no_target)).to eq "Unknown"
-    end
-  end
-
-  describe "#sale price" do
-
-    let(:variant) {create(:base_variant)}
-    let(:sale_variant) {create(:variant_in_sale)}
-
-    it "finds a sale price of an item if applicable" do
-      expect(subject.send(:sale_price, variant)).to eq nil
-      expect(subject.send(:sale_price, sale_variant)).to eq 6
-    end
-  end
-
-  describe "#safe colour" do
-    let(:variant) {mock_model(Spree::Variant)}
-    let(:option_value) {mock_model(Spree::OptionValue, presentation: 'foobar')}
-
-    let(:variant_with_no_color) {mock_model(Spree::Variant)}
-    let(:option_value_with_no_color) {mock_model(Spree::OptionValue)}
-
-    before do
-      option_value.stub_chain([:option_type, :is_color?], true)
-      variant.stub(option_values: [option_value])
-
-      option_value_with_no_color.stub_chain([:option_type, :is_color?], false)
-      variant_with_no_color.stub(option_values: [option_value_with_no_color])
+  describe '#gender' do
+    it "returns 'Women' for target Women" do
+      expect(subject.send(:gender, suite)).to eq 'Women'
     end
 
-    it "detects an option type's color" do
-      expect(subject.send(:safe_colour, variant)).to eq 'foobar'
-      expect(subject.send(:safe_colour, variant_with_no_color)).to eq nil
+    it "returns 'Men' for target Men" do
+      target.name = "Men"
+      expect(subject.send(:gender, suite)).to eq 'Men'
     end
-  end
 
-  describe "#find_product_page_url" do
-    let(:variant) {mock_model(Spree::Variant)}
-    let(:product_page) {create(:product_page)}
-    let(:tab) {[double]}
-
-    before do
-      tab.stub(url_safe_tab_type: 'made-by-the-gang')
-      variant.stub(id: 12)
-      variant.stub(is_master?: nil)
-      product_page.stub(permalink: 'rainbow-sweater')
-    end
-    it "find the product page url" do
-      expect(subject.send(:find_product_page_url, product_page, tab, variant)).to eq 'http://www.example.com/shop/items/rainbow-sweater/made-by-the-gang/12'
+    it "returns 'Unisex' when target is not supplied" do
+      suite.target = nil
+      expect(subject.send(:gender, suite)).to eq 'Unisex'
     end
   end
 
   describe "#variant_image_url" do
-
-    let(:variant) {create(:variant)}
-    let(:product_page) {create(:product_page)}
-    let(:tab) {[double]}
-    let(:image) { mock_model(Spree::Image) }
-
+    let(:image) { Spree::Image.new }
 
     context 'image on variant' do
       before do
-        variant.stub(images_for: ([image]))
+        allow(variant).to receive(:images_for).with(target).and_return [image]
         image.stub_chain(:attachment, :url).and_return('image-url')
       end
 
       it "returns image on variant" do
-        expect(subject.send(:variant_image_url, variant, product_page, tab)).to eq 'image-url'
+        expect(subject.send(:variant_image_url, variant, suite, tab)).to eq 'image-url'
       end
     end
 
     context 'when no image on variant' do
       before do
-        variant.stub(images_for: [])
-        variant.stub_chain(:product, :images_for).and_return([image])
+        allow(product).to receive(:images_for).with(target).and_return [image]
         image.stub_chain(:attachment, :url).and_return('image-url-on-product')
       end
 
       it "finds image url on product" do
-        expect(subject.send(:variant_image_url, variant, product_page, tab)).to eq 'image-url-on-product'
+        expect(subject.send(:variant_image_url, variant, suite, tab)).to eq 'image-url-on-product'
       end
     end
 
-    context 'when n image on variant and product' do
+    context 'when there is no image on variant and product' do
+      let(:suite_tab_image) { Spree::SuiteTabImage.new }
       before do
-        variant.stub(images_for: [])
-        variant.stub_chain(:product, :images_for).and_return([])
-        tab.stub(image: image)
-        image.stub_chain(:attachment, :url).and_return('image-url-on-tab')
+        tab.image = suite_tab_image
+        suite_tab_image.stub_chain(:attachment, :url).and_return('image-url-on-tab')
       end
+
       it "finds image on tab" do
-        expect(subject.send(:variant_image_url, variant, product_page, tab)).to eq 'image-url-on-tab'
+        expect(subject.send(:variant_image_url, variant, suite, tab)).to eq 'image-url-on-tab'
       end
     end
 
@@ -169,7 +123,7 @@ context
         tab.stub(image: nil)
       end
       it "if found no where else, returns default image" do
-        expect(subject.send(:variant_image_url, variant, product_page, tab)).to eq Spree::PolyvorFeedJob::DEFAULT_IMAGE_URL
+        expect(subject.send(:variant_image_url, variant, suite, tab)).to eq Spree::PolyvorFeedJob::DEFAULT_IMAGE_URL
       end
     end
   end
