a644629bd5ae905f9bf7eb360c7a596917fafb3e
Martin Tomov <martin@railsdog.com>
2014-12-09 15:34:40 +0000

    Remove comment
    Former-commit-id: 57e5f28512d616065a3c1c69a10329b826b765c2 [formerly 011f74ffd5e59adc998e21fd07243152f95b6564 [formerly f9b76ce04abf0e18fad22cb032079997524e5426]]
    Former-commit-id: b0832f94bc345a67c3c5cecdf119f812e8c0bae9
    Former-commit-id: 833669d5e061367b012a77b87ad73e1358f9b108 [formerly dc69c44dbdfd6aa1863e0a1ca362b25bcfb72c98]
    Former-commit-id: 5232651bd6a84033e2ff9533907eff84a293366e

--- core/app/models/spree/assembly_definition_variant.rb
+++ core/app/models/spree/assembly_definition_variant.rb
@@ -2,7 +2,7 @@ class Spree::AssemblyDefinitionVariant < ActiveRecord::Base
   acts_as_paranoid
   belongs_to :variant, class_name: "Spree::Variant", foreign_key: "variant_id"
   belongs_to :assembly_definition_part, touch: true, class_name: "Spree::AssemblyDefinitionPart", foreign_key: "assembly_definition_part_id"
-  belongs_to :assembly_product, class_name: "Spree::Product", foreign_key: "assembly_product_id"#, touch: true
+  belongs_to :assembly_product, class_name: "Spree::Product", foreign_key: "assembly_product_id"
 
   validates_presence_of :variant_id, :assembly_definition_part_id
 
