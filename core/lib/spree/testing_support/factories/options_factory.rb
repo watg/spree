f637fa977407b9ff56414f045aecde0d390aad3e
Rosie-Brigham <rosie@woolandthegang.com>
2014-12-10 15:36:54 +0000

    creates capybara tests for adding products to cart
    Former-commit-id: 558485db07d29bbc974ecf304d793f5b3d33baf2 [formerly b398577fb71c7137bf72cbf14547628957b76dc4 [formerly 0e81dd47adf758ec7f312d126c8efcbbf74ebeea]]
    Former-commit-id: 921274057675112a28c5a737036405726d5a4406
    Former-commit-id: ca6364804f08f3f9f34b200aeacb07e441a2b8dc [formerly 4f3848c560b375094a0e85139bd90d0fa28f5c9f]
    Former-commit-id: 721d9f88f076c210ac8726cc8d73dfaab4936bbd

--- core/lib/spree/testing_support/factories/options_factory.rb
+++ core/lib/spree/testing_support/factories/options_factory.rb
@@ -8,8 +8,18 @@ FactoryGirl.define do
 
   factory :option_value, class: Spree::OptionValue do
     sequence(:name) {|e| "hot-pink#{e}"}
-    presentation 'Hot Pink'
     option_type
+
+    after(:build) do |i,evaluator|
+      if evaluator.presentation.nil?
+        if evaluator.name
+          i.presentation = evaluator.name
+        else
+          i.presentation = 'Hot Pink'
+        end
+      end
+    end
+
   end
 
   factory :option_type, class: Spree::OptionType do
@@ -17,4 +27,5 @@ FactoryGirl.define do
     presentation 'Color'
     sku_part 'COL'
   end
+
 end
