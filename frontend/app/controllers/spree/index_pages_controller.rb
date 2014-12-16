b9d89c178891c1f52ccfe88db96cb44f98498975
Martin Tomov <mtomov@outlook.com>
2014-12-16 14:49:24 +0000

    Allow taxon pages to render deleted taxons.
    Add script to restore collection taxonss
    
    
    Former-commit-id: c294be958ce4338705719b86af10af4bdca82e98 [formerly 60342c2276850f4ba7609c831f14f28801d56ca3 [formerly 42ce3fb4b3be449852c0abb6e52d68c9d56e48a3]]
    Former-commit-id: 2712a34ecb7dcd93ad7fcb80dc1db8f5e5127805
    Former-commit-id: ab36b8312f8166c63cd8cc56a4266828740ba78b [formerly 5d079a917579653d433fbaa6185af994a7a7bb2a]
    Former-commit-id: a3981c51bc4c0676b2c816fbfa8b60622ef0fc8c

--- frontend/app/controllers/spree/index_pages_controller.rb
+++ frontend/app/controllers/spree/index_pages_controller.rb
@@ -10,6 +10,39 @@ module Spree
       'whats-new-women' => 'whats-new/women',
       'whats-new-men' => 'whats-new/men',
       'whats-new-seen-in-press' => 'whats-new/seen-in-press',
+
+      'whats-new-seen-in-press' => 'whats-new/seen-in-press',
+
+      # old top level static pages. Can't redirect due to routes preference
+      # 'knitting/men' => 'knit-kits/men',
+      # 'knitting/women' => 'knit-kits/women',
+      # 'knitting/kids' => 'knit-kits/kids',
+
+      'knitting/beginner-knit-kits' => 'knit-kits/beginner-kits',
+      'knitting/easy-knit-kits' => 'knit-kits/easy-kits',
+      'knitting/intermediate-knit-kits' => 'knit-kits/intermediate-kits',
+      'knitting/advanced-knit-kits' => 'knit-kits/advanced-kits',
+
+      'knitting/cotton' => 'yarn-and-patterns/yarn/cotton',
+      'knitting/wool-and-cotton' => 'yarn-and-patterns/yarn/wool-and-merino-wool',
+      'knitting/patterns' => 'yarn-and-patterns/patterns',
+      'knitting/needles' => 'yarn-and-patterns/supplies/needles',
+
+      'knitting/mens-knit-kits' => 'knit-kits/men',
+      'knitting/womens-knit-kits' => 'knit-kits/women',
+      'knitting/summer-knit-projects' => 'knit-kits/spring-slash-summer-kits',
+      'knitting/winter-knit-projects' => 'knit-kits/autumn-slash-winter-kits',
+      'knitting/kids/mini-gang' => 'knit-kits/mini-gang',
+      'knitting/kids/baby-gang' => 'knit-kits/baby-gang',
+      'knitting/knit-kits' => 'knit-kits',
+      'knitting-bag-knit-kits' => 'knit-kits/bags',
+      'knitting/new-arrivals' => 'knit-kits/new-arrivals',
+
+      # 3 left:
+      # /knitting-bag-knit-kits
+      # /knitting/knit-your-own
+      # /knitting/new-arrivals
+
     }
 
     def show
@@ -25,7 +58,7 @@ module Spree
 
       permalink = Spree::IndexPagesController::TAXON_ALIASES[params[:id]] || params[:id]
 
-      taxon = Spree::Taxon.where(permalink: permalink).first
+      taxon = Spree::Taxon.with_deleted.where(permalink: permalink).first
       if taxon
         redirect_to spree.nested_taxons_path(taxon), status: :moved_permanently
       else
