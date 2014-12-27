15bc4aadb9a65cf2e0d405c8f923bfcbd3337b07
David Dawson <david.dawson@gmail.com>
2014-12-27 10:27:27 +0000

    Update the pricing of free shipping
    Former-commit-id: f940ceedbce7faf022b8e144a51d998768ceab09 [formerly 6b3067943b9d2849d5ba824fbd5014d5ac3818a1 [formerly bdf3bc8d225301e613b95d766337604f8c9dd73c]]
    Former-commit-id: 4238dfcd768e84ba5b9c495c5426b35e389756bd
    Former-commit-id: 57611cb79d3f25e6d723ab80d94404f6723a4136 [formerly 8ea6837b900586e4016b02f8a2b84008dc1d62f1]
    Former-commit-id: ddd00031b7b6b7850f6afeb5719201a447e73025

--- frontend/app/helpers/spree/free_shipping_helper.rb
+++ frontend/app/helpers/spree/free_shipping_helper.rb
@@ -2,10 +2,10 @@ module Spree
   module FreeShippingHelper
 
     FREE_SHIPPING_COUNTRY_CODES = {
-      'US' => '$110.00',
-      'GB' => '£75.00',
-      'DE' => '€65.00',
-      'FR' => '€65.00',
+      'US' => '$150.00',
+      'GB' => '£100.00',
+      'DE' => '€100.00',
+      'FR' => '€100.00',
       'ES' => '€70.00'
       #'CA' => '$250.00'
       #'CH' => '$250.00'
