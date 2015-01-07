fb07072719c76d570f3a58b4f603b333130749a0
Martin Tomov <mtomov@outlook.com>
2015-01-07 11:59:30 +0000

    Move #number_of_shipments_pending out of a variant decorator
    and into StockItem
    
    
    Former-commit-id: 08a04ce731405d6fbb655145570939b553d048d5 [formerly d9495eb007b44648cb6157aa0db52347279b4bd0 [formerly 70327d76e1e76e2de2cca346d6ff482f91a4d6ef]]
    Former-commit-id: cc4e388040c6e201b7257a9141ef0a2ac65bad5a
    Former-commit-id: 498b2a05c996c7004ee695184f4c05b9592cf46f [formerly b353444962ff35b83ba213519f0cc2bedea3e9f3]
    Former-commit-id: 1c89d5449571f0208d8c4e5a9f796e6d19093752

--- core/spec/models/spree/stock_item_spec.rb
+++ core/spec/models/spree/stock_item_spec.rb
@@ -426,4 +426,84 @@ describe Spree::StockItem do
       end.to change { subject.variant.updated_at }
     end
   end
+
+  describe "#number_of_shipments_pending" do
+    let(:variant) { subject.variant }
+
+    let(:order) do
+      order = create(:order)
+      order.state = 'complete'
+      order.completed_at = Time.now
+      order.tap(&:save!)
+    end
+
+    let(:shipment) do
+      shipment = Spree::Shipment.new
+      shipment.stock_location = stock_location
+      shipment.shipping_methods << create(:shipping_method)
+      shipment.order = order
+      # We don't care about this in this test
+      shipment.stub(:ensure_correct_adjustment)
+      shipment.tap(&:save!)
+    end
+
+    let!(:unit) do
+      unit = shipment.inventory_units.build
+      #unit.state = 'backordered'
+      unit.variant_id = stock_item.variant.id
+      unit.order_id = order.id
+      unit.pending = false
+      unit.tap(&:save!)
+    end
+
+    it "takes into account pending false" do
+      expect(stock_item.number_of_shipments_pending).to eq 1
+    end
+
+    it "takes into account pending true" do
+      unit.pending = true
+      unit.save
+      expect(stock_item.number_of_shipments_pending).to eq 0
+    end
+
+    it "is influenced by order states" do
+      order.cancel!
+      expect(stock_item.number_of_shipments_pending).to eq 0
+      order.resume!
+      expect(stock_item.number_of_shipments_pending).to eq 1
+    end
+
+    context "supplier" do
+      let(:supplier) { create(:supplier) }
+
+      before do
+        unit.supplier = supplier
+        unit.save
+      end
+
+      it "take no supplier" do
+        stock_item.supplier = nil
+        expect(stock_item.number_of_shipments_pending).to eq 0
+      end
+
+      it "takes into account a supplier" do
+        stock_item.supplier = supplier
+        expect(stock_item.number_of_shipments_pending).to eq 1
+      end
+
+    end
+
+    context "stock_location" do
+      let(:another_stock_location) { create(:stock_location) }
+
+      before do
+        stock_item.stock_location = another_stock_location
+      end
+
+      it "takes  account a stock_locations" do
+        expect(stock_item.number_of_shipments_pending).to eq 0
+      end
+    end
+  end # #number_of_shipments_pending
+
 end
