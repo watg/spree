2e4fa5ff27c333b4c29d381afdae1f5aba7a2b88
Layon Ferreira <layon@woolandthegang.com>
2015-02-25 18:41:27 +0000

    improve todays sells method
    Former-commit-id: 690ade40c9aad1640018a44ca038a1112c438c06 [formerly d21dc3f17c388bc25c95cc8d463e926ae63a8b78]
    Former-commit-id: 2f241d02198047a17b46739441236b902915f611

--- api/app/controllers/spree/api/dashboard_controller.rb
+++ api/app/controllers/spree/api/dashboard_controller.rb
@@ -1,50 +1,49 @@
 module Spree
   module Api
     class DashboardController < Spree::Api::BaseController
-
       def last_bought_product
-        last_variant=Spree::Order.where('completed_at is not null').last.variants.last
-        last_product=last_variant.product
+        last_variant = Spree::Order.where('completed_at is not null').last.variants.last
+        last_product = last_variant.product
         variant_image_url = last_variant.images.first.attachment.url if last_variant.images.any?
-        response={name: last_product.name, marketing_type: last_product.marketing_type.title ,image_url: variant_image_url}
+        response = { name: last_product.name, marketing_type: last_product.marketing_type.title, image_url: variant_image_url }
         respond_to do |format|
-          format.json { render :json => response.to_json }
+          format.json { render json: response.to_json }
         end
       end
 
       def today_sells
-        today_sells={EUR: 0, GBP: 0, USD: 0}.merge(
-          Spree::Order.where("completed_at >= ?", Time.zone.now.beginning_of_week).group('currency').sum(:total)
+        today_sells = { EUR: 0, GBP: 0, USD: 0 }.merge(
+          Spree::Order.where('completed_at >= ?', Time.zone.now.beginning_of_day).group('currency').sum(:total)
         )
         respond_to do |format|
-          format.json { render :json => today_sells.to_json }
+          format.json { render json: today_sells.to_json }
         end
       end
 
       def today_orders
-        today_orders={ total: Spree::Order.where("completed_at >= ?", Time.zone.now.beginning_of_day).count }
+        today_orders = { total: Spree::Order.where('completed_at >= ?', Time.zone.now.beginning_of_day).count }
         respond_to do |format|
-          format.json { render :json => today_orders.to_json }
+          format.json { render json: today_orders.to_json }
         end
       end
 
       def today_sells_by_type
-        sells_by_type=query_sells_by_type.sort_by{ |k| k.last }.reverse!
+        sells_by_type = query_sells_by_type.sort_by(&:last).reverse!
 
         respond_to do |format|
-          format.json { render :json => sells_by_type.to_json }
+          format.json { render json: sells_by_type.to_json }
         end
       end
 
       def today_orders_by_hour
-        today_orders= Spree::Order.where("completed_at >= ?", Time.zone.now.beginning_of_day).group_by_hour(:completed_at,range: Date.today..Time.now).count.to_a
+        today_orders = Spree::Order.where('completed_at >= ?', Time.zone.now.beginning_of_day).group_by_hour(:completed_at, range: Date.today..Time.now).count.to_a
 
         today_orders.map! do |point|
-          {x: point[0].to_i, y: point[1]}
+          { x: point[0].to_i, y: point[1] }
         end
 
         respond_to do |format|
-          format.json { render :json => today_orders.to_json }
+          format.json { render json: today_orders.to_json }
         end
       end
 
@@ -52,13 +51,10 @@ module Spree
 
       def query_sells_by_type
         data = Spree::LineItem.joins(:order).merge(Spree::Order.complete)
-                 .where('spree_orders.completed_at > ?', Time.zone.now.beginning_of_day )
-                 .joins(variant: [product: :marketing_type])
-                 .group('spree_marketing_types.title').count
-        merged_data=Spree::MarketingType.all.map(&:title).compact.inject({}) { |hash,mt| hash[mt] = data[mt] || 0; hash }
-
-        Spree::MarketingType.all.map(&:title).compact.inject({}) { |hash,mt| hash[mt] = data[mt] || 0; hash }
-        merged_data.to_a
+               .where('spree_orders.completed_at > ?', Time.zone.now.beginning_of_day)
+               .joins(variant: [product: :marketing_type])
+               .group('spree_marketing_types.title').count
+        data.sort.reverse!
       end
     end
   end
