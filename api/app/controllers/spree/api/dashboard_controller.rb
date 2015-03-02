41e94f78f89108aa2dcfdc491fcf9ab217343550
Layon Ferreira <layonferreira@ddawsons-iMac.home>
2015-03-02 10:35:02 +0000

    filter orders to show only orders from ecom
    Former-commit-id: fdae0427741453be726a27c59544eb3211510843 [formerly 98bd92666548f93cfa3bdd92df364cd75092f073]
    Former-commit-id: 225505a0241c3e93068692e94589e5efec5db606

--- api/app/controllers/spree/api/dashboard_controller.rb
+++ api/app/controllers/spree/api/dashboard_controller.rb
@@ -14,7 +14,7 @@ module Spree
 
       def today_sells
         today_sells = { EUR: 0, GBP: 0, USD: 0 }.merge(
-          Spree::Order.where('completed_at >= ?', Time.zone.now.beginning_of_day).group('currency').sum(:total)
+          today_valid_orders.group('currency').sum(:total)
         )
 
         respond_to do |format|
@@ -23,7 +23,7 @@ module Spree
       end
 
       def today_orders
-        today_orders = { total: Spree::Order.where('completed_at >= ?', Time.zone.now.beginning_of_day).count }
+        today_orders = { total: today_valid_orders.count }
 
         respond_to do |format|
           format.json { render json: today_orders.to_json }
@@ -31,8 +31,7 @@ module Spree
       end
 
       def today_items
-        today_items = Spree::LineItem.joins(:order).merge(Spree::Order.complete)
-                      .where('spree_orders.completed_at > ?', Time.zone.now.beginning_of_day)
+        today_items = Spree::LineItem.joins(:order).merge(today_valid_orders)
                       .sum(:quantity)
 
         respond_to do |format|
@@ -49,7 +48,7 @@ module Spree
       end
 
       def today_orders_by_hour
-        today_orders = Spree::Order.where('completed_at >= ?', Time.zone.now.beginning_of_day).group_by_hour(:completed_at, range: Date.today..Time.zone.now.beginning_of_hour-1).count.to_a
+        today_orders = today_valid_orders.group_by_hour(:completed_at, range: Date.today..Time.zone.now.beginning_of_hour-1).count.to_a
 
         today_orders.map! do |point|
           { x: point[0].to_i, y: point[1] }
@@ -63,11 +62,16 @@ module Spree
       private
 
       def query_sells_by_type
-        data = Spree::LineItem.joins(:order).merge(Spree::Order.complete)
-               .where('spree_orders.completed_at > ?', Time.zone.now.beginning_of_day)
+        data = Spree::LineItem.joins(:order).merge(today_valid_orders)
                .joins(variant: [product: :marketing_type])
                .group('spree_marketing_types.title').sum(:quantity)
         data.sort.reverse!
+      end
+
+      def today_valid_orders
+        Spree::Order.complete.where("completed_at > ?", Time.zone.now.at_beginning_of_day).where.not(
+          email: 'request@woolandthegang.com').joins(payments: :payment_method).merge(
+          Spree::PaymentMethod.where(name: ["Credit Card", "PayPal"])).uniq
       end
     end
   end
