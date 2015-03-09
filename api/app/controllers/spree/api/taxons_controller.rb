343a2bf12a5b95934347a337080e5faa9e834cde
Martin Tomov <mtomov@outlook.com>
2015-03-09 10:29:49 +0000

    Increase listed suites for a taxon on the backend from 500 to 700
    Former-commit-id: bef1eb5a2329ec04d523cf5b3c2a6624c65c6cec [formerly 9a00ec25afc114b45eb87b59e1c50069c24b86be]
    Former-commit-id: 34f049f384f7ca42aa93cecf85c13f524b08ef1f

--- api/app/controllers/spree/api/taxons_controller.rb
+++ api/app/controllers/spree/api/taxons_controller.rb
@@ -66,7 +66,7 @@ module Spree
         # Products#index does not do the sorting.
         taxon = Spree::Taxon.find(params[:id])
         @suites = taxon.suites.includes(:image).ransack(params[:q]).result
-        @suites = @suites.page(params[:page]).per(params[:per_page] || 500)
+        @suites = @suites.page(params[:page]).per(params[:per_page] || 700)
         render "spree/api/suites/index"
       end
 
