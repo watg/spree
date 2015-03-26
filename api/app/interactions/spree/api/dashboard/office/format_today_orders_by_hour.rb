cf93a8e5e0d63c47033000facf515911da47bd23
Layon Ferreira <layon@woolandthegang.com>
2015-03-26 17:58:49 +0000

    Move date range from constant to instance variable
    Former-commit-id: 6ae8f41d039ba8dd7e252fd4a0f1da23a6ae4e05 [formerly 2de1482c52946d57d15c279cd075708ab5f6a003]
    Former-commit-id: d3d2cb71dbbc6eb16b85ddb4091a80875cacc43d

--- api/app/interactions/spree/api/dashboard/office/format_today_orders_by_hour.rb
+++ api/app/interactions/spree/api/dashboard/office/format_today_orders_by_hour.rb
@@ -5,14 +5,14 @@ module Spree
         # returns a formatted version of the number of
         # orders by marketing type for the dashboard api
         class FormatTodayOrdersByHour
-          TODAY_RANGE = Time.zone.now.at_beginning_of_day..Time.zone.now.beginning_of_hour - 1
           def initialize(valid_orders = Spree::Order.complete.not_cancelled)
             @orders = valid_orders
+            @range = Time.zone.now.at_beginning_of_day..Time.zone.now.beginning_of_hour - 1
           end
 
           def run
             today_orders = Spree::Api::Dashboard::Office::FindTodayValidOrders.new(@orders).run
-            grouped_orders = today_orders.group_by_hour(:completed_at, range: TODAY_RANGE).count
+            grouped_orders = today_orders.group_by_hour(:completed_at, range: @range).count
             grouped_orders.map do |k, v|
               { x: k.to_i, y: v }
             end
