d2a24aa37236d15347167ac7b730a0dd41da07eb
Layon Ferreira <layon@woolandthegang.com>
2015-03-31 12:11:00 +0100

    refactor search class
    Former-commit-id: 0aa9b876b4abbd0ed16fce20af2d92550a6b9f09 [formerly de14d47a87ae174c1e80f5cdb285c126d438f4fa]
    Former-commit-id: 71a4a848a07356084f424fbbc889647719f32ea6

--- frontend/app/services/search/base.rb
+++ frontend/app/services/search/base.rb
@@ -1,80 +1,42 @@
 module Search
   # Base search class for finding suites
   class Base
-    attr_accessor :properties
-    attr_accessor :current_user
-    attr_accessor :current_currency
-
     def initialize(params)
-      @current_currency = Spree::Config[:currency]
-      @properties = {}
-      prepare(params)
+      prepare_properties(params)
     end
 
-    def retrieve_suites
-      @suites_scope = get_base_scope
-      curr_page = page || 1
-
-      @suites = @suites_scope
-      @suites = @suites.page(curr_page).per(per_page)
+    def results
+      @results ||= filter_suites(Spree::Suite.active, @keywords).page(@page).per(@per_page)
     end
 
-    def method_missing(name)
-      if @properties.has_key? name
-        @properties[name]
-      else
-        super
-      end
+    def num_pages
+      (results.count.to_i + @per_page.to_i - 1) / @per_page.to_i
     end
 
-  protected
-    def get_base_scope
-      base_scope = Spree::Suite.active
-      # base_scope = base_scope.in_taxon(taxon) unless taxon.blank? # not used at present
-      base_scope = get_suites_conditions_for(base_scope, @properties[:keywords])
-      # base_scope = add_search_scopes(base_scope) # not used at present
-      base_scope
+    private
+
+    def prepare_properties(params)
+      @keywords = params.key?(:keywords) ? params[:keywords] : nil
+      @per_page = if params.key?(:per_page)
+                    params[:per_page].to_i
+                  else
+                    Spree::Config[:products_per_page].to_i
+                  end
+      @page = params.key?(:page) && params[:page].to_i > 0 ? params[:page] : 1
     end
 
-    def add_search_scopes(base_scope)
-      search.each do |name, scope_attribute|
-        scope_name = name.to_sym
-        if base_scope.respond_to?(:search_scopes) && base_scope.search_scopes.include?(scope_name.to_sym)
-          base_scope = base_scope.send(scope_name, *scope_attribute)
-        else
-          base_scope = base_scope.merge(Spree::Suite.ransack({scope_name => scope_attribute}).result)
-        end
-      end if search
-      base_scope
+    def prepare_query(query)
+      query.split.join("&")
     end
 
-    # method should return new scope based on base_scope
-    def get_suites_conditions_for(base_scope, query)
+    def filter_suites(base_scope, query)
       unless query.blank?
-        query = sanitize(query)
-        base_scope = base_scope.joins(:indexed_search).where("indexed_searches.document @@ to_tsquery('english',:q)", q: query)
+        query = prepare_query(query)
+        base_scope = base_scope
+                     .joins(:indexed_search)
+                     .where("indexed_searches.document @@ to_tsquery('english',:q)", q: query)
       end
       base_scope
     end
-
-    def prepare(params)
-      @properties[:taxon] = params[:taxon].blank? ? nil : Spree::Taxon.find(params[:taxon])
-      @properties[:keywords] = params[:keywords]
-      @properties[:indexed_search] = params[:indexed_search]
-
-      per_page = params[:per_page].to_i
-      @properties[:per_page] = per_page > 0 ? per_page : Spree::Config[:products_per_page]
-      @properties[:page] = (params[:page].to_i <= 0) ? 1 : params[:page].to_i
-    end
-
-
-    def sanitize(query)
-      join_with_and_logic(query.split)
-    end
-
-    def join_with_and_logic(query)
-      query.join('&')
-    end
-
   end
 end
--- frontend/spec/services/search/base_spec.rb
+++ frontend/spec/services/search/base_spec.rb
@@ -2,69 +2,70 @@ require "spec_helper"
 
 module Search
   describe Base do
-    let!(:suite1) { create(:suite, :with_tab, title: "RoR Mug") }
-    let!(:suite2) { create(:suite, :with_tab, title: "RoR Shirt") }
-    let!(:taxon) { create(:taxon, name: "Ruby on Rails") }
+    context "correct filters" do
+      let!(:suite1) { create(:suite, :with_tab, title: "RoR Mug") }
+      let!(:suite2) { create(:suite, :with_tab, title: "RoR Shirt") }
+      let!(:taxon) { create(:taxon, name: "Ruby on Rails") }
 
-    before do
-      suite1.taxons << taxon
-      suite2.taxons << taxon
+      before do
+        suite1.taxons << taxon
+        suite2.taxons << taxon
+      end
+
+      it "returns all suites with tabs by default" do
+        create(:suite, title: "RoR Shirt2")
+        params = {}
+
+        searcher = described_class.new(params)
+        expect(searcher.results.count).to eq(2)
+      end
+
+      it "switches to next page according to the page parameter" do
+        create(:suite, :with_tab, title: "RoR Pants")
+
+        params = { per_page: "2" }
+        searcher = described_class.new(params)
+        expect(searcher.results.count).to eq(2)
+
+        params.merge! page: "2"
+        searcher = described_class.new(params)
+        expect(searcher.results.count).to eq(1)
+      end
+
+      it "returns suites matching the keywords using AND logic" do
+        IndexedSearch.rebuild
+        params = { keywords: "ror mug" }
+        searcher = described_class.new(params)
+        expect(searcher.results.count).to eq(1)
+      end
+
+      it "returns suites within taxons matching the keywords" do
+        suite2.taxons.clear
+        IndexedSearch.rebuild
+        params = { keywords: "Rails" }
+        searcher = described_class.new(params)
+        expect(searcher.results.count).to eq(1)
+      end
+
+      it "returns suites with keywords matching both suite and taxons" do
+        IndexedSearch.rebuild
+        params = { keywords: "ruby shirt" }
+        searcher = described_class.new(params)
+        expect(searcher.results.count).to eq(1)
+      end
     end
-
-    it "returns all suites with tabs by default" do
-      suite3 = create(:suite, title: "RoR Shirt2")
-      params = {}
-
-      searcher = described_class.new(params)
-      expect(searcher.retrieve_suites.count).to eq(2)
-    end
-
-    it "switches to next page according to the page parameter" do
-      suite3 = create(:suite, :with_tab, title: "RoR Pants")
-
-      params = { per_page: "2" }
-      searcher = described_class.new(params)
-      expect(searcher.retrieve_suites.count).to eq(2)
-
-      params.merge! page: "2"
-      searcher = described_class.new(params)
-      expect(searcher.retrieve_suites.count).to eq(1)
-    end
-
-    # it "uses ransack if scope not found" do
-    #   params = { :per_page => "",
-    #              :search => { "title_not_cont" => "Shirt" }}
-    #   searcher = described_class.new(params)
-    #   expect(searcher.retrieve_suites.count).to eq(1)
-    # end
-
-    it "accepts a current user" do
-      user = double
-      searcher = described_class.new({})
-      searcher.current_user = user
-      expect(searcher.current_user).to eql(user)
-    end
-
-    it "returns suites matching the keywords using AND logic" do
-      IndexedSearch.rebuild
-      params = { keywords: "ror mug" }
-      searcher = described_class.new(params)
-      expect(searcher.retrieve_suites.count).to eq(1)
-    end
-
-    it "returns suites within taxons matching the keywords" do
-      suite2.taxons.clear
-      IndexedSearch.rebuild
-      params = { keywords: "Rails" }
-      searcher = described_class.new(params)
-      expect(searcher.retrieve_suites.count).to eq(1)
-    end
-
-    it "returns suites with keywords matching both suite and taxons" do
-      IndexedSearch.rebuild
-      params = { keywords: "ruby shirt" }
-      searcher = described_class.new(params)
-      expect(searcher.retrieve_suites.count).to eq(1)
+    context "#num_pages" do
+      it "returns the total pages" do
+        params = { keywords: "" }
+        searcher = described_class.new(params)
+        allow(searcher).to receive(:results).and_return([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+        searcher.instance_variable_set("@per_page", 10)
+        expect(searcher.num_pages).to eq(1)
+        searcher.instance_variable_set("@per_page", 5)
+        expect(searcher.num_pages).to eq(2)
+        searcher.instance_variable_set("@per_page", 1)
+        expect(searcher.num_pages).to eq(10)
+      end
     end
   end
 end
