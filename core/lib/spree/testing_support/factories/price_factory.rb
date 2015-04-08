f394f791fe1402399676451dce327f505a6b77aa
Layon Ferreira <layon@woolandthegang.com>
2015-04-08 16:31:39 +0100

    create price presenter
    Former-commit-id: 152f2d445b70e129cc25cf8c7063e64c729c938b [formerly 7f6441a57a9f2688683ca429c478695938c38260]
    Former-commit-id: 482f11cc6e4f3bf6b9f748d290a2722942b219a0

--- core/lib/spree/testing_support/factories/price_factory.rb
+++ core/lib/spree/testing_support/factories/price_factory.rb
@@ -1,8 +1,11 @@
 FactoryGirl.define do
   factory :price, class: Spree::Price do
     amount 19.99
+    sale_amount 9.99
+    part_amount 1.99
     currency 'USD'
     is_kit false
     sale false
+    variant
   end
 end
