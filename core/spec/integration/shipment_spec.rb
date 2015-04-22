50bd91fd3e82b4a3455bf489a9f23ecea5a0829a
namtax <namtax@hotmail.com>
2015-04-22 10:42:30 +0100

    shipment_spec: fix failing spec
    Former-commit-id: 6873c7ee83c78c0296273a393337eeed44849376 [formerly 0e1a5b801e94224bfe0e65671ecc769fc9de1e1d]
    Former-commit-id: 46c8a17748db3a5eb9804bf81f7501fcf9e0bc70

--- core/spec/integration/shipment_spec.rb
+++ core/spec/integration/shipment_spec.rb
@@ -7,18 +7,18 @@ describe "user receives email" do
   let(:kit)       { create(:product_type, name: "kit") }
 
   before do
-    product = line_item.product
-    product.product_type = kit
-    product.save
+    line_item.product.product_type = kit
     shipment.state = "ready"
     allow(shipment).to receive_messages determine_state: "shipped"
+    Timecop.freeze
   end
 
   context "kit and pattern survey email" do
+    let(:handler) { YAML.load(Delayed::Job.last.handler).object }
+
     it "creates a delayed job for 1 month after today" do
-      Timecop.freeze
       shipment.update!(order)
-      expect(Delayed::Job.last.handler).to include("KitAndPatternEmailSurveyJob")
+      expect(handler).to be_a(Shipping::KitAndPatternEmailSurveyJob)
       expect(Delayed::Job.last.run_at).to eq(1.month.from_now)
     end
   end
