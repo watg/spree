488fe81dba1d228a32beeed8856b307e684188df
namtax <namtax@hotmail.com>
2015-04-22 11:50:03 +0100

    shipment_spec: compare times by second precision - to fix failing spec on ci
    Former-commit-id: 51d283eff11b2b3d94a1943bcfbf5acdef2a649f [formerly a68fd9fbb1864e7b81101d6bcd6c37e6d6be7f73]
    Former-commit-id: 544f6102a6b37fa86f5a86db74caa21a4d0eb132

--- core/spec/integration/shipment_spec.rb
+++ core/spec/integration/shipment_spec.rb
@@ -19,7 +19,7 @@ describe "user receives email" do
     it "creates a delayed job for 1 month after today" do
       shipment.update!(order)
       expect(handler).to be_a(Shipping::KitAndPatternMailer)
-      expect(Delayed::Job.last.run_at).to eq(1.month.from_now)
+      expect(Delayed::Job.last.run_at.to_s).to eq(1.month.from_now.to_s)
     end
   end
 end
