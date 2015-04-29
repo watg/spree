9174e81497ad4fee27fd6e38e518bfbddc16a078
namtax <namtax@hotmail.com>
2015-04-29 17:48:58 +0100

    knitting_experience_email_spec: explicitly check that knitting experience gets sent in integration test
    Former-commit-id: 6cccc4c77f079be6493a47e94020d9d8fb0c6909 [formerly d2be38f8bb402f0eacdc1834aefdf5a48dcf05d3]
    Former-commit-id: 1a938d9320cd1df22a24789145953611f00901eb

---
+++ core/spec/integration/knitting_experience_email_spec.rb
@@ -0,0 +1,29 @@
+require "spec_helper"
+
+describe "user receives email" do
+  let(:order)       { create(:order) }
+  let(:shipment)    { create(:shipment, order: order) }
+  let(:line_item)   { create(:line_item, order: order) }
+  let(:kit)         { create(:product_type, :kit) }
+  let(:job)         { Delayed::Job.last }
+  let(:handler)     { YAML.load(job.handler).object }
+  let(:mail_header) { ActionMailer::Base.deliveries.last.header }
+  let(:template)    { mail_header['X-MC-Template'].value }
+  let(:recipient)   { JSON.load(mail_header['X-MC-MergeVars'].value)['name'] }
+
+  before do
+    line_item.product.update(product_type: kit)
+    shipment.state = "ready"
+    allow(shipment).to receive_messages determine_state: "shipped"
+    Timecop.freeze
+  end
+
+  it "dispatches email in 1 month" do
+    shipment.update!(order)
+    job.invoke_job
+    expect(job.run_at.to_s).to eq(1.month.from_now.to_s)
+    expect(handler).to be_a(Shipping::KnittingExperienceMailer)
+    expect(template).to eq 'en_kits_and_patterns_survey'
+    expect(recipient).to eq shipment.address.full_name
+  end
+end
