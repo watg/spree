131ad3ced6c06bdb680b5a3d346dda9ed48c1e3c
Rosie Brigham <rosie@woolandthegang.com>
2015-05-05 16:25:23 +0100

    adds some spaces
    Former-commit-id: 5b10b0f07e36ad7f3a453c3d12c41284e5fdb8f4 [formerly a03502566c5e621bf6c7679ee02ba3c9ac037b75]
    Former-commit-id: 8ccea6995a41256d726d83a1b15aa47e163472a5

--- frontend/spec/presenters/suite_tab_presenter_spec.rb
+++ frontend/spec/presenters/suite_tab_presenter_spec.rb
@@ -339,7 +339,9 @@ describe Spree::SuiteTabPresenter do
   end
 
   describe "#cross_sales_heading" do
+
     context "when tab type is yarn-and-wool" do
+
       before { tab.tab_type = 'yarn-and-wool' }
       it "returns correct heading" do
         expect(subject.cross_sales_heading).to eq 'What you can make'
@@ -347,17 +349,21 @@ describe Spree::SuiteTabPresenter do
     end
 
     context "when tab type is knitting-pattern" do
+
       before { tab.tab_type = 'knitting-pattern' }
       it "returns correct heading" do
         expect(subject.cross_sales_heading).to eq 'Knit this pattern'
       end
     end
+
     context "when tab type is made-by-the-gang" do
+
       before { tab.tab_type = 'made-by-the-gang' }
       it "returns correct heading" do
         expect(subject.cross_sales_heading).to eq 'More ready made goodness'
       end
     end
+
     context "when tab type is anything else" do
       it "returns correct heading" do
         expect(subject.cross_sales_heading).to eq 'More WATG goodness'
