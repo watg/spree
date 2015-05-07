904e07ea7ab5fa149b46c9ea23aaf222ce6d2f1b
Steve Edwards <thecolonyroom@Steves-MacBook-Air.local>
2015-05-07 16:48:28 +0100

    Using constants.
    Former-commit-id: 2d920b7d56457d6cf1047dd6a3fa2902ff2ec3c1 [formerly 07f955c9275a562ff6c65d4827e891dd34471278]
    Former-commit-id: f140c7d3ced99695c35cb585e83c285a2fc2341c

--- frontend/app/presenters/spree/suite_tab_presenter.rb
+++ frontend/app/presenters/spree/suite_tab_presenter.rb
@@ -5,6 +5,14 @@ module Spree
     attr_accessor :suite, :variants
     delegate :cross_sales, to: :suite_tab
 
+    X_SALE_HEADING = {
+      'yarn-and-wool' => 'What you can make',
+      'knitting-pattern' => 'Knit this pattern',
+      'made-by-the-gang' => 'More ready made goodness'
+    }
+
+    X_SALE_DEFAULT = 'More WATG goodness'
+
     def initialize(object, template, context={})
       super(object, template, context)
       @suite = context[:suite]
@@ -103,14 +111,7 @@ module Spree
     end
 
     def cross_sales_heading
-      heading = {
-        'yarn-and-wool' => 'What you can make',
-        'knitting-pattern' => 'Knit this pattern',
-        'made-by-the-gang' => 'More ready made goodness'
-      }
-
-      default = 'More WATG goodness'
-      heading.fetch(tab_type, default)
+      X_SALE_HEADING.fetch(tab_type, X_SALE_DEFAULT)
     end
 
     private
