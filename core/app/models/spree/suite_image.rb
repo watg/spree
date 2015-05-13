547bba2f7264b33ca8f2db1bab3e853744113f1d
Layon Ferreira <lferreira@lferreira-SATELLITE-L755.(none)>
2015-05-13 13:08:48 +0100

    add search size for suite_images
    Former-commit-id: 1f856397d5bef4c3a3fbf15af473be3373cefe89 [formerly b3175447d1600af358a0f4bc0186d7808013b110]
    Former-commit-id: 65bfa7f1ee4a5fd95159e56c5bfe7a029ea42a6c

--- core/app/models/spree/suite_image.rb
+++ core/app/models/spree/suite_image.rb
@@ -2,13 +2,16 @@ module Spree
   class SuiteImage < Image
 
     has_attached_file :attachment,
-      :styles        => { large: "640x900>", small: '310x396>', mobile: '150x192>' },
-      :default_style => :large,
-      :convert_options =>  {
-        :all => '-strip -auto-orient',
-        :large => "-quality 80"
-      },
-      :keep_old_files => false
+                      :styles => { large: "640x900>",
+                                   small: '310x396>',
+                                   mobile: '150x192>',
+                                   search: '35x45>' },
+                      :default_style => :large,
+                      :convert_options => {
+                        :all => '-strip -auto-orient',
+                        :large => "-quality 80"
+                      },
+                      :keep_old_files => false
 
   end
 end
