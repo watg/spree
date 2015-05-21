44eaf0e8264eb6c787e2fd99679c7c80c7cfcd39
Layon Ferreira <layon@woolandthegang.com>
2015-05-21 11:03:33 +0100

    edit mark as old runner, to copy the prices to the new table
    Former-commit-id: a8789d3382220f1dae1949eca691fbbe1015bd86 [formerly 4030e3faec2ab47332079f4acd2f7964d97ee572]
    Former-commit-id: 1097191b4e5f6a14cea0e0b282afd6733ffb4bef

---
+++ core/app/models/spree/price_new.rb
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+module Spree
+  class PriceNew < Spree::Base
+    acts_as_paranoid
+    belongs_to :variant, class_name: 'Spree::Variant', inverse_of: :prices, touch: true
+
+    CURRENCY_SYMBOL = {'USD' => '$', 'GBP' => '£', 'EUR' => '€'}
+    TYPES = [:normal,:normal_sale,:part,:part_sale]
+
+
+    validate :check_price
+    validates :amount, numericality: { greater_than_or_equal_to: 0 }, allow_nil: false
+    validate :validate_amount_maximum
+
+    after_save :trigger_suite_tab_cache_rebuilder, :unless => :skip_callbacks
+
+    # Prevent duplicate prices from happening in the system, there is also a uniq
+    # index on the database table to ensure there are no race conditions
+    validates_uniqueness_of :variant_id, :scope => [ :currency, :sale, :is_kit, :deleted_at ]
+
+    class << self
+
+      def money(amount, currency)
+        Spree::Money.new(amount || 0, { currency: currency })
+      end
+
+      def default_price
+        new(amount: 0, currency: Spree::Config[:currency], sale: false, is_kit: false)
+      end
+
+      def find_normal_prices(prices, currency=nil)
+        prices = prices.select{ |price| price.sale == false && price.is_kit == false }
+        prices = prices.select{ |price| price.currency == currency } if currency
+        prices
+      end
+
+      def find_normal_price(prices, currency=nil)
+        find_normal_prices(prices, currency).first
+      end
+
+      def find_sale_prices(prices, currency=nil)
+        prices = prices.select{ |price| price.sale == true && price.is_kit == false }
+        prices = prices.select{ |price| price.currency == currency } if currency
+        prices
+      end
+
+      def find_sale_price(prices, currency=nil)
+        find_sale_prices(prices, currency).first
+      end
+
+      def find_part_price(prices, currency)
+        prices.select{ |price| price.currency == currency && price.sale == false && price.is_kit == true }.first
+      end
+
+      def find_part_sale_price(prices, currency)
+        prices.select{ |price| price.currency == currency && price.sale == true && price.is_kit == true }.first
+      end
+    end
+
+    def display_amount
+      money
+    end
+
+    alias :display_price :display_amount
+
+    def money
+      self.class.money(amount,currency)
+    end
+
+    def price
+      amount
+    end
+
+    def in_subunit
+      ( (price || 0) * 100 ).to_i
+    end
+
+    def currency_symbol
+      CURRENCY_SYMBOL[currency.to_s.upcase]
+    end
+
+    def price=(price)
+      self[:amount] = Spree::LocalizedNumber.parse(price)
+    end
+
+    # Remove variant default_scope `deleted_at: nil`
+    def variant
+      Spree::Variant.unscoped { super }
+    end
+
+    private
+
+    def trigger_suite_tab_cache_rebuilder
+      Spree::SuiteTabCacheRebuilder.rebuild_from_variant_async(self.variant)
+    end
+
+    def check_price
+      self.currency ||= Spree::Config[:currency]
+    end
+
+    def maximum_amount
+      BigDecimal '999999.99'
+    end
+
+    def validate_amount_maximum
+      if amount && amount > maximum_amount
+        errors.add :amount, I18n.t('errors.messages.less_than_or_equal_to', count: maximum_amount)
+      end
+    end
+  end
+end
