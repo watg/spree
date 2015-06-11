12a9e76c480d3c01117bd7dd82bc842f50110de2
David Dawson <david.dawson@gmail.com>
2015-06-11 14:42:49 +0100

    Rubocop order_factory
    Former-commit-id: 0dbf622336434185b14a0892dcd7927b4afa7e17 [formerly a554c5284fab2f9635827419b10ad0f0c85669be]
    Former-commit-id: 946c41a5bf1fad65ef41a2a472ef441364eddb51

--- core/lib/spree/testing_support/factories/order_factory.rb
+++ core/lib/spree/testing_support/factories/order_factory.rb
@@ -6,7 +6,7 @@ FactoryGirl.define do
     completed_at nil
     email { user.email }
     created_at Time.now
-    currency 'USD'
+    currency "USD"
 
     association :order_type, factory: :order_type, strategy: :build
 
@@ -30,19 +30,22 @@ FactoryGirl.define do
       end
 
       trait :with_marketing_type do
-        after(:create) do |order, evaluator|
-          order.line_items.first.variant.product.update_column(:marketing_type_id, create(:marketing_type).id)
+        after(:create) do |order, _evaluator|
+          order.line_items.first.variant.product.update_column(
+            :marketing_type_id, create(:marketing_type).id)
         end
       end
 
       trait :with_product_group do
-        after(:create) do |order, evaluator|
-          order.line_items.first.variant.product.update_column(:product_group_id, create(:product_group).id)
+        after(:create) do |order, _evaluator|
+          order.line_items.first.variant.product.update_column(
+            :product_group_id, create(:product_group).id)
         end
       end
 
       after(:create) do |order, evaluator|
-        order.line_items = create_list(:line_item, evaluator.line_items_count, order: order, price: evaluator.line_items_price)
+        order.line_items = create_list(
+          :line_item, evaluator.line_items_count, order: order, price: evaluator.line_items_price)
 
         order.shipments << create(:shipment, order: order, cost: evaluator.shipment_cost)
 
@@ -50,7 +53,7 @@ FactoryGirl.define do
       end
 
       factory :completed_order do
-        state 'complete'
+        state "complete"
 
         transient do
           completed_at Time.now
@@ -62,7 +65,7 @@ FactoryGirl.define do
       end
 
       factory :completed_order_with_totals do
-        state 'complete'
+        state "complete"
 
         transient do
           completed_at Time.now
@@ -79,48 +82,47 @@ FactoryGirl.define do
         end
 
         factory :order_ready_to_ship do
-          payment_state 'paid'
-          shipment_state 'ready'
+          payment_state "paid"
+          shipment_state "ready"
 
           after(:create) do |order|
-            create(:payment, amount: order.total, order: order, state: 'completed')
+            create(:payment, amount: order.total, order: order, state: "completed")
             order.shipments.each do |shipment|
-              shipment.inventory_units.update_all state: 'on_hand'
-              shipment.update_column('state', 'ready')
+              shipment.inventory_units.update_all state: "on_hand"
+              shipment.update_column("state", "ready")
             end
             order.reload
           end
         end
 
         factory :shipped_order do
-          payment_state 'paid'
-          shipment_state 'shipped'
+          payment_state "paid"
+          shipment_state "shipped"
 
           after(:create) do |order|
-            create(:payment, amount: order.total, order: order, state: 'completed')
+            create(:payment, amount: order.total, order: order, state: "completed")
             order.shipments.each do |shipment|
-              shipment.inventory_units.update_all state: 'shipped'
-              shipment.update_column('state', 'shipped')
+              shipment.inventory_units.update_all state: "shipped"
+              shipment.update_column("state", "shipped")
             end
             order.reload
           end
         end
 
         factory :order_ready_to_be_consigned_and_allocated do
-          payment_state 'paid'
-          shipment_state 'ready'
+          payment_state "paid"
+          shipment_state "ready"
           after(:create) do |order|
             create(:parcel, order: order, box_id: create(:box).id)
             create(:parcel, order: order, box_id: create(:box).id)
             order.shipments.each do |shipment|
               shipment.inventory_units.each do |u|
-                u.update_columns(state: 'on_hand', supplier_id: create(:supplier))
+                u.update_columns(state: "on_hand", supplier_id: create(:supplier))
               end
-              shipment.update_column('state', 'ready')
+              shipment.update_column("state", "ready")
             end
             order.reload
           end
-
         end
       end
     end
