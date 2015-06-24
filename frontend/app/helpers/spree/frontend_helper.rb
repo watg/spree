b21c8e6470896acb039953c3f249362f8aeb2ac4
Steve Edwards <thecolonyroom@Steves-MacBook-Air.local>
2015-06-24 15:56:04 +0100

    New mobile hesder and navigation.
    Former-commit-id: ececb3a2bc683b4d069148d9eff76dce7d832cd0 [formerly 77c7be19627bb6c1c09bc563d01078fe9aefe6d4]
    Former-commit-id: 09fba0e1f15a0195dd39d0eea6e3d3199c0004dd

--- frontend/app/helpers/spree/frontend_helper.rb
+++ frontend/app/helpers/spree/frontend_helper.rb
@@ -1,6 +1,16 @@
 module Spree
   module FrontendHelper
 
+    def my_bag_link_mobile()
+      if simple_current_order.nil? or simple_current_order.item_count.zero?
+        text = "0"
+      else
+        text = "#{simple_current_order.item_count}"
+      end
+
+      link_to text.html_safe, spree.cart_path, :class => "link-cart"
+    end
+
     def my_bag_link(text = nil)
       text = text ? h(text) : Spree.t('cart')
       css_class = nil
