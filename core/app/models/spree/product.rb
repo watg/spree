34c75484f87079ed67b9c84d3c0a2c82c991bff1
john barnes <namtax@hotmail.com>
2015-06-25 10:44:03 +0100

    assembly_definition_part: associate kit product via product_id not assembly_product_id
    Former-commit-id: 0b408475b511e679ce616a8839edf0d107e71a5d [formerly 8a7a6ba105537b39625fad042f58290f09c32b8d]
    Former-commit-id: 4c374c86ad5dcb35f6784583735354fcfaeb57ff

--- core/app/models/spree/product.rb
+++ core/app/models/spree/product.rb
@@ -65,7 +65,7 @@ module Spree
 
     has_many :assembly_definition_parts, foreign_key: "part_id"
 
-    has_many :product_parts, foreign_key: "assembly_product_id", class_name: "Spree::AssemblyDefinitionPart"
+    has_many :product_parts, foreign_key: "product_id", class_name: "Spree::AssemblyDefinitionPart"
     has_many :part_products, foreign_key: "part_id", class_name: "Spree::AssemblyDefinitionPart"
 
     has_many :parts, through: :product_parts
