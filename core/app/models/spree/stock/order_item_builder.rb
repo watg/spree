8718377c912022834d151edea40d362bb9cf2ca6
john barnes <namtax@hotmail.com>
2015-07-02 15:09:30 +0100

    order_item_builder: build items for ready to wear products with parts
    Former-commit-id: a779b1c3edb4ecef17a67f25bfe350b01ad578c0 [formerly bafa7b23417fa649b159c955cbda898d0d0ba185]
    Former-commit-id: 9aa8cfd6b4728f5d3346b10e154242218888ffc7

--- core/app/models/spree/stock/order_item_builder.rb
+++ core/app/models/spree/stock/order_item_builder.rb
@@ -6,46 +6,48 @@ module Spree::Stock
 
     def initialize(line_items)
       @line_items = line_items
-      @items = []
-      build_items
+      @items      = build_items
     end
 
     def group_variants
-      hash = {}
-      items.group_by(&:variant_id).map do |variant_id, units|
+      items.group_by(&:variant_id).map.inject({}) do |hash, (variant_id, units)|
         hash[variant_id] = units.sum(&:quantity)
+        hash
       end
-      hash
     end
 
     def find_by_variant_id(variant_id)
-      items.select { |item| item.variant_id == variant_id }
+      items.select{ |item| item.variant_id == variant_id }
     end
 
     def variant_ids_for_line_item(line_item)
-      items.select { |item| item.line_item == line_item }.map(&:variant_id)
+      items.select{ |item| item.line_item == line_item }.map(&:variant_id)
     end
 
   private
 
     def build_items
-      line_items.uniq.each do |line_item|
-        build_item(line_item)
-      end
+      line_items.uniq.map do |line_item|
+        [build_item(line_item), build_items_from_parts(required_parts(line_item))]
+      end.flatten.compact
     end
 
     def build_item(line_item)
-      parts = line_item.parts.select {|part| !part.container? }
+      Item.new(line_item.variant_id, line_item, line_item.quantity) unless kit(line_item)
+    end
 
-      if parts.any?
-        parts.each do |part|
-          @items << Item.new(part.variant_id, line_item, part.quantity * line_item.quantity)
-        end
-      else
-        @items << Item.new(line_item.variant_id, line_item, line_item.quantity)
+    def build_items_from_parts(parts)
+      parts.map do |part|
+        Item.new(part.variant_id, part.line_item, part.quantity * part.line_item.quantity)
       end
     end
 
+    def required_parts(line_item)
+      line_item.parts.reject{ |part| part.container? }
+    end
 
+    def kit(line_item)
+      line_item.variant.product.product_type.kit?
+    end
   end
 end
