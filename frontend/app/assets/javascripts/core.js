8e2b1b938ce7145ca0466de158c297266773095c
Layon Ferreira <layon@woolandthegang.com>
2015-07-09 15:49:21 +0100

    Fix ipad/iphone double tap issue
    Former-commit-id: e701d4850611deeb4aab83fa0d73dd57813c1a39 [formerly 7460b6877887b75c375e6baa7ab1fc1266bc33ba]
    Former-commit-id: 7b6c2a0a2fd1bd5c9b27c536869a511058cc03d5

--- frontend/app/assets/javascripts/core.js
+++ frontend/app/assets/javascripts/core.js
@@ -14,6 +14,9 @@ var readyCore = function() {
   core.readyCustomScroll();
   core.readyOlapic();
   core.readyContactModal();
+  if (core.isMobileWidthOrLess() === true) {
+    core.hoverTouchUnstick();
+  }
 };
 
 $(document).on('page:load ready', readyCore);
@@ -415,6 +418,28 @@ core.signupGetPromoText = function() {
 // optional, depending where veronica wants the disclaimer
 core.signupGetPromoDisclaimer = function() {
   return 'Available only until October 21st';
+}
+
+core.hoverTouchUnstick = function() {
+  // Check if the device supports touch events
+  if('ontouchstart' in document.documentElement) {
+    // Loop through each stylesheet
+    for(var sheetI = document.styleSheets.length - 1; sheetI >= 0; sheetI--) {
+      var sheet = document.styleSheets[sheetI];
+      // Verify if cssRules exists in sheet
+      if(sheet.cssRules) {
+        // Loop through each rule in sheet
+        for(var ruleI = sheet.cssRules.length - 1; ruleI >= 0; ruleI--) {
+          var rule = sheet.cssRules[ruleI];
+          // Verify rule has selector text
+          if(rule.selectorText) {
+            // Replace hover psuedo-class with active psuedo-class
+            rule.selectorText = rule.selectorText.replace(":hover", ":active");
+          }
+        }
+      }
+    }
+  }
 }
 
 core.clearFocus = function() {
