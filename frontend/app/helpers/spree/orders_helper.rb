0e1abc13449aadfbd28999e3715fb35dba1e804a
Layon Ferreira <layon@woolandthegang.com>
2015-07-15 10:56:35 +0100

    remove old linkshare pixel
    Former-commit-id: 9431a3a88692f637a8aac255a242841ba2032db1 [formerly c93b3862f4fc12279ff6937a8a82b95780ce2241]
    Former-commit-id: 2787620d39579ff697802f7d88bb5221f3f8b7fe

--- frontend/app/helpers/spree/orders_helper.rb
+++ frontend/app/helpers/spree/orders_helper.rb
@@ -5,16 +5,6 @@ module Spree
   module OrdersHelper
     include TruncateHtmlHelper
 
-    def linkshare_url(order)
-      query_string = order.decorate.linkshare_params.
-        inject([]) {|s, t|
-          s << [t[0], t[1].join('|') ].join("="); s
-        }.
-        join('&')
-
-      [LinkShare.base_url, query_string].join
-    end
-
     def order_just_completed?(order)
       flash[:order_completed] && order.present?
     end
--- frontend/spec/helpers/orders_helper_spec.rb
+++ frontend/spec/helpers/orders_helper_spec.rb
@@ -2,16 +2,6 @@ require 'spec_helper'
 
 describe Spree::OrdersHelper do
   subject     { Object.extend(Spree::OrdersHelper) }
-  let(:linkshare_config) { URI(LinkShare.base_url) }
-  let(:order) { decorated_order = double(linkshare_params: {a: ["foo"], b: ["bar"]}) ; double(decorate: decorated_order) }
-
-  it "builds linkshare_url" do
-    uri = URI(subject.linkshare_url(order))
-    expect(uri.scheme).to eq linkshare_config.scheme
-    expect(uri.host).to   eq linkshare_config.host
-    expect(uri.path).to   eq linkshare_config.path
-    expect(uri.query).to  eq "a=foo&b=bar"
-  end
 
   it "truncates HTML correctly in product description" do
     product = double(:description => "<strong>" + ("a" * 95) + "</strong> This content is invisible.")
