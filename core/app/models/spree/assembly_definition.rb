f406422c977d0ac8b66b104c0de6d5e3815ef44c
David Dawson <david.dawson@gmail.com>
2015-07-21 20:13:22 +0100

    Remove old models and tables ( assembly_definitions )
    Former-commit-id: d849fc4063f42582baa1169fccb79727d46f3c37 [formerly 17743beb2fda95b2326044dcca177582f80872d3]
    Former-commit-id: 359601fc74553ec83100e1a6e5de83a2f4b6cb6c

--- core/app/models/spree/assembly_definition.rb
+++
@@ -1,6 +0,0 @@
-class Spree::AssemblyDefinition < ActiveRecord::Base
-  acts_as_paranoid
-  belongs_to :variant, class_name: "Spree::Variant"
-  belongs_to :assembly_product, class_name: "Spree::Product", foreign_key: "assembly_product_id"
-  has_many :images, -> { order(:position) }, as: :viewable, dependent: :destroy, class_name: "Spree::AssemblyDefinitionImage"
-end
