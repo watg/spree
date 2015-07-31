87788b5044d1810c7ebb543ccbefeef215456e7c
john barnes <namtax@hotmail.com>
2015-07-31 10:38:45 +0100

    shipping_method_controller_spec: spec fix

--- backend/spec/controllers/spree/admin/shipping_methods_controller_spec.rb
+++ backend/spec/controllers/spree/admin/shipping_methods_controller_spec.rb
@@ -3,12 +3,11 @@ require 'spec_helper'
 describe Spree::Admin::ShippingMethodsController, :type => :controller do
   stub_authorization!
 
-  # Regression test for #1240
+  let(:shipping_method) { create(:shipping_method) }
+
   it "should not hard-delete shipping methods" do
-    shipping_method = stub_model(Spree::ShippingMethod)
-    allow(Spree::ShippingMethod).to receive_messages :find => shipping_method
     expect(shipping_method.deleted_at).to be_nil
-    spree_delete :destroy, :id => 1
+    spree_get :destroy, :id => shipping_method.id
     expect(shipping_method.reload.deleted_at).not_to be_nil
   end
 end
